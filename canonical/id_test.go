c93da733414aecb4ed5135e56fef5cd28e6154b8
John Bass <John_Bass2@cable.comcast.com>
2016-04-20 11:15:15 -0700

    Added tests

--- canonical/id_test.go
+++ canonical/id_test.go
@@ -1,87 +1,46 @@
-// Package canonical provides a simple normalization for device identifiers.  These
-// identifiers have the form {prefix}:{id}/{service}/{ignored}, where only {prefix}
-// and {id} are required.
 package canonical
 
 import (
-	"errors"
-	"fmt"
-	"regexp"
-	"strings"
-	"unicode"
+	"testing"
 )
 
-// Id represents a normalized identifer for a device.
-type Id interface {
-	// Bytes returns a distinct byte slice representation
-	// of this canonicalized identifier.
-	Bytes() []byte
-}
-
-// id is the internal Id implementation
-type id string
-
-func (this id) Bytes() []byte {
-	return []byte(this)
-}
-
-var _ Id = id("")
-
-const (
-	hexDigits     = "0123456789abcdefABCDEF"
-	macDelimiters = ":-.,"
-	macPrefix     = "mac"
-	macLength     = 12
-)
-
-var (
-	// idPattern is the precompiled regular expression that all device identifiers must match.
-	// Matching is partial, as everything after the service is ignored.
-	idPattern = regexp.MustCompile(
-		`^(?P<prefix>(?i)mac|uuid|dns|serial):(?P<id>[^/]+)(?P<service>/[^/]+)?`,
-	)
-)
-
-// ParseId parses a raw string identifier into an Id
-func ParseId(value string) (Id, error) {
-	match := idPattern.FindStringSubmatch(value)
-	if match == nil {
-		return nil, errors.New(fmt.Sprintf("Invalid device id: %s", value))
+func TestParseId(t *testing.T) {
+	testData := []struct {
+		deviceId string
+		expected string
+		valid    bool
+	}{
+		{"MAC:11:22:33:44:55:66", "mac:112233445566", true},
+		{"MAC:11aaBB445566", "mac:11aabb445566", true},
+		{"mac:11-aa-BB-44-55-66", "mac:11aabb445566", true},
+		{"mac:11,aa,BB,44,55,66", "mac:11aabb445566", true},
+		{"uuid:anything Goes!", "uuid:anything Goes!", true},
+		{"dns:anything Goes!", "dns:anything Goes!", true},
+		{"serial:1234", "serial:1234", true},
+		{"mac:11-aa-BB-44-55-66/service", "mac:11aabb445566/service/", true},
+		{"mac:11-aa-BB-44-55-66/service/", "mac:11aabb445566/service/", true},
+		{"mac:11-aa-BB-44-55-66/service/ignoreMe", "mac:11aabb445566/service/", true},
+		{"mac:11-aa-BB-44-55-66/service/foo/bar", "mac:11aabb445566/service/", true},
+		{"invalid:a-BB-44-55", "", false},
+		{"mac:11-aa-BB-44-55", "", false},
+		{"MAC:invalid45566", "", false},
 	}
 
-	var (
-		prefix  = strings.ToLower(match[1])
-		idPart  = match[2]
-		service = match[3]
-	)
+	for _, record := range testData {
+		id, err := ParseId(record.deviceId)
 
-	if prefix == macPrefix {
-		var invalidCharacter rune = -1
-		idPart = strings.Map(
-			func(r rune) rune {
-				switch {
-				case strings.ContainsRune(hexDigits, r):
-					return unicode.ToLower(r)
-				case strings.ContainsRune(macDelimiters, r):
-					return -1
-				default:
-					invalidCharacter = r
-					return -1
-				}
-			},
-			idPart,
-		)
+		if err != nil {
+			if record.valid {
+				t.Errorf("Unexpected error for %s", record.deviceId)
+			}
+		} else {
+			if !record.valid {
+				t.Fatalf("Expected error for %s", record.deviceId)
+			}
 
-		if invalidCharacter != -1 {
-			return nil, errors.New(fmt.Sprintf("Invalid character in mac: %c", invalidCharacter))
-		} else if len(idPart) != macLength {
-			return nil, errors.New(fmt.Sprintf("Invalid length of mac: %s", idPart))
+			if actual := string(id.Bytes()); actual != record.expected {
+				t.Errorf("For %s, ParseId() returned %s, but was expecting %s", record.deviceId, actual, record.expected)
+			}
 		}
 	}
-
-	if len(service) > 0 {
-		return id(fmt.Sprintf("%s:%s%s/", prefix, idPart, service)), nil
-	}
-
-	return id(fmt.Sprintf("%s:%s", prefix, idPart)), nil
 }
