dee6acc11609a7b974bd2143a6cd50e95b191f16
John Bass <John_Bass2@cable.comcast.com>
2016-05-07 11:30:51 -0700

    Added NewErrorLog(); refactored parameter order for consistency

--- context/logger.go
+++ context/logger.go
@@ -3,6 +3,7 @@ package context
 import (
 	"fmt"
 	"io"
+	"log"
 	"net"
 	"net/http"
 )
@@ -82,9 +83,14 @@ func (logger DefaultLogger) Error(parameters ...interface{}) {
 	logger.doWrite("ERROR", parameters...)
 }
 
+// NewErrorLog creates a new log.Logger appropriate for http.Server.ErrorLog
+func NewErrorLog(logger Logger, serverName string) *log.Logger {
+	return log.New(&ErrorWriter{logger}, serverName, log.LstdFlags|log.LUTC)
+}
+
 // NewConnectionStateLogger produces a function appropriate for http.Server.ConnState.
 // The returned function will log debug statements for each state change.
-func NewConnectionStateLogger(serverName string, logger Logger) func(net.Conn, http.ConnState) {
+func NewConnectionStateLogger(logger Logger, serverName string) func(net.Conn, http.ConnState) {
 	return func(connection net.Conn, connectionState http.ConnState) {
 		logger.Debug(
 			"[%s] [%s] -> %s",
