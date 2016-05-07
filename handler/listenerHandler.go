c9dbb12d0283ba9c77626550fb4695dd284ea956
John Bass <John_Bass2@cable.comcast.com>
2016-05-07 11:54:57 -0700

    Added a ChainHandler that dispatches request events

---
+++ handler/listenerHandler.go
@@ -0,0 +1,50 @@
+package handler
+
+import (
+	"github.com/Comcast/webpa-common/context"
+	"net/http"
+)
+
+// RequestListener gets notified of certain high-level request events
+type RequestListener interface {
+	// RequestReceived is invoked anytime a handler receives a request
+	RequestReceived(*http.Request)
+
+	// RequestCompleted is invoked after the response has been written.
+	// The first parameter is the response status code, e.g. http.StatusOk
+	RequestCompleted(int, *http.Request)
+}
+
+// ListenableResponseWriter wraps a http.ResponseWriter and records the
+// status code.
+type listenableResponseWriter struct {
+	http.ResponseWriter
+	statusCode int
+}
+
+func (writer *listenableResponseWriter) WriteHeader(statusCode int) {
+	writer.ResponseWriter.WriteHeader(statusCode)
+	writer.statusCode = statusCode
+}
+
+// Listen produces a ChainHandler that notifies the given list of listeners
+// of request events.
+func Listen(listeners ...RequestListener) ChainHandler {
+	return ChainHandlerFunc(func(logger context.Logger, response http.ResponseWriter, request *http.Request, next http.Handler) {
+		for _, listener := range listeners {
+			listener.RequestReceived(request)
+		}
+
+		listenableResponse := &listenableResponseWriter{ResponseWriter: response}
+		next.ServeHTTP(listenableResponse, request)
+
+		statusCode := listenableResponse.statusCode
+		if statusCode < 1 {
+			statusCode = http.StatusOK
+		}
+
+		for _, listener := range listeners {
+			listener.RequestCompleted(statusCode, request)
+		}
+	})
+}
