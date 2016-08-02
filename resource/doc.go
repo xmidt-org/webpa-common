51b2bb32497bd289a405a4e86672215d0618e9ad
John Bass <John_Bass2@cable.comcast.com>
2016-08-01 23:02:13 -0700

    Expanded a package comment

--- resource/doc.go
+++ resource/doc.go
@@ -1,4 +1,8 @@
 /*
-Package resource provides common resource-handling functionality
+Package resource provides common resource-handling functionality.
+
+This package is heavily inspired by frameworks like Spring.  The concept
+is that external resources, such as files or HTTP content, can be configured
+via JSON and then consumed via a common interface.
 */
 package resource
