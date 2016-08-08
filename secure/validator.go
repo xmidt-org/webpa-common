85ce74d33f19b743ed94e1bbc0d0a7623ebb7f70
John Bass <John_Bass2@cable.comcast.com>
2016-08-07 20:37:19 -0700

    Refactored the Validator interface; fleshed out support for the SAT tokens

--- secure/validator.go
+++ secure/validator.go
@@ -1,66 +1,82 @@
 package secure
 
 import (
-	"errors"
-	"github.com/Comcast/webpa-common/store"
-	"github.com/SermoDigital/jose/crypto"
+	"fmt"
+	"github.com/Comcast/webpa-common/secure/key"
 	"github.com/SermoDigital/jose/jws"
 	"github.com/SermoDigital/jose/jwt"
-)
-
-var (
-	// DidNotMatch is returned by ExactMatch validators to indicate that
-	// the token did not match the expected value.
-	DidNotMatch = errors.New("The secure token did not match")
 )
 
 // Validator describes the behavior of a type which can validate tokens
 type Validator interface {
 	// Validate asserts that the given token is valid, most often verifying
-	// the credentials in the token.  This method can return an arbitrary result
-	// understood by the caller.
-	Validate(*Token) (interface{}, error)
+	// the credentials in the token.  A separate error is returned to indicate
+	// any problems during validation, such as the inability to access a network resource.
+	// In general, the contract of this method is that a Token passes validation
+	// if and only if it returns BOTH true and a nil error.
+	Validate(*Token) (bool, error)
 }
 
 // ValidatorFunc is a function type that implements Validator
-type ValidatorFunc func(*Token) (interface{}, error)
+type ValidatorFunc func(*Token) (bool, error)
 
-func (v ValidatorFunc) Validate(token *Token) (interface{}, error) {
+func (v ValidatorFunc) Validate(token *Token) (bool, error) {
 	return v(token)
 }
 
-// ExactMatch produces a closure which validates that a token matches
+// ValidateExactMatch produces a closure which validates that a token matches
 // a given value exactly.  This validator simply returns the original
 // token as the result along with a possible error indicating that the
 // match was a failure.
-func ExactMatch(match string) Validator {
-	return ValidatorFunc(func(token *Token) (interface{}, error) {
-		if match != token.value {
-			return token, DidNotMatch
-		}
-
-		return token, nil
+func ValidateExactMatch(match string) Validator {
+	return ValidatorFunc(func(token *Token) (bool, error) {
+		return match == token.value, nil
 	})
 }
 
-// Verify returns a Validator closure that will verify JWT tokens using a given
-// key, signing method, and JWT-specific validators.
-func Verify(key store.Value, method crypto.SigningMethod, validators ...*jwt.Validator) Validator {
-	return ValidatorFunc(func(token *Token) (interface{}, error) {
-		jwt, err := jws.ParseJWT(token.Bytes())
+// ValidateJWS produces a Validator for JWS tokens.  Comcast SAT tokens are JWS tokens.
+func ValidateJWS(resolver key.Resolver, defaultKeyId string, jwtValidators ...*jwt.Validator) Validator {
+	return ValidatorFunc(func(token *Token) (bool, error) {
+		if token.Type() != Bearer {
+			return false, nil
+		}
+
+		jwtToken, err := jws.ParseJWT(token.Bytes())
 		if err != nil {
-			return nil, err
+			return false, err
 		}
 
-		key, err := key.Load()
+		jwsToken, ok := jwtToken.(jws.JWS)
+		if !ok {
+			return false, fmt.Errorf("Token is not a valid JWS: %s", token)
+		}
+
+		header := jwsToken.Protected()
+		signingMethodName, ok := header.Get("alg").(string)
+		if !ok {
+			return false, fmt.Errorf("Token does not define a signing method in the header: %s", token)
+		}
+
+		signingMethod := jws.GetSigningMethod(signingMethodName)
+		if signingMethod == nil {
+			return false, fmt.Errorf("Unknown signing method: %s", signingMethodName)
+		}
+
+		keyId, ok := header.Get("kid").(string)
+		if len(keyId) == 0 {
+			keyId = defaultKeyId
+		}
+
+		key, err := resolver.ResolveKey(keyId)
 		if err != nil {
-			return jwt, err
+			return false, fmt.Errorf("Unable to resolve key with id: %s", keyId)
 		}
 
-		if err := jwt.Validate(key, method, validators...); err != nil {
-			return jwt, err
+		// Have to invoke Validate on the JWT type due to bad interface design in SermoDigital
+		if err := jwtToken.Validate(key, signingMethod, jwtValidators...); err != nil {
+			return false, err
 		}
 
-		return jwt, nil
+		return true, nil
 	})
 }
