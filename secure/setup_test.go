d62caef0e23cbdbba2c17d2776fd6ea8132b54e5
John Bass <John_Bass2@cable.comcast.com>
2016-08-24 16:35:48 -0700

    Generate JWT per test; basic exact match test

--- secure/setup_test.go
+++ secure/setup_test.go
@@ -2,7 +2,11 @@ package secure
 
 import (
 	"fmt"
+	"github.com/Comcast/webpa-common/resource"
+	"github.com/Comcast/webpa-common/secure/key"
+	"github.com/SermoDigital/jose/crypto"
 	"github.com/SermoDigital/jose/jws"
+	"github.com/SermoDigital/jose/jwt"
 	"os"
 	"testing"
 )
@@ -14,13 +18,72 @@ const (
 
 var (
 	publicKeyFileURI  string
-	privateKeyFileURI string
+	publicKeyResolver key.Resolver
+
+	privateKeyFileURI  string
+	privateKeyResolver key.Resolver
+
+	// ripped these test claims from the SATS swagger example
+	testClaims = jws.Claims{
+		"valid": true,
+		"scope": []string{"x1:capability:read"},
+		"allowedResources": map[string]interface{}{
+			"allowedDeviceIds":         []string{"1641529834193109183"},
+			"allowedPartners":          []string{"comcast, cox"},
+			"allowedServiceAccountIds": []string{"4924346887352567847"},
+		},
+	}
+
+	testJWT           jwt.JWT
+	testSerializedJWT []byte
 )
 
 func TestMain(m *testing.M) {
-	currentDirectory, err := os.Getwd()
-	if err != nil {
-		fmt.Fprintf(os.Stderr, "Unable to obtain current working directory: %v\n", err)
-		return
-	}
+	os.Exit(func() int {
+		currentDirectory, err := os.Getwd()
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to obtain current working directory: %s\n", err)
+			return 1
+		}
+
+		publicKeyFileURI = fmt.Sprintf("%s/%s", currentDirectory, publicKeyFileName)
+		privateKeyFileURI = fmt.Sprintf("%s/%s", currentDirectory, privateKeyFileName)
+
+		privateKeyResolver, err = (&key.ResolverFactory{
+			Factory: resource.Factory{URI: privateKeyFileURI},
+			Purpose: key.PurposeSign,
+		}).NewResolver()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to create private key resolver: %s\n", err)
+			return 1
+		}
+
+		publicKeyResolver, err = (&key.ResolverFactory{
+			Factory: resource.Factory{URI: publicKeyFileURI},
+			Purpose: key.PurposeVerify,
+		}).NewResolver()
+
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to create public key resolver: %s\n", err)
+			return 1
+		}
+
+		privateKey, err := privateKeyResolver.ResolveKey("")
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to resolve private key: %s\n", err)
+			return 1
+		}
+
+		// generate a unique JWT for each run of the tests
+		// this also exercises our secure/key infrastructure
+		testJWT = jws.NewJWT(testClaims, crypto.SigningMethodRS256)
+		testSerializedJWT, err = testJWT.Serialize(privateKey)
+		if err != nil {
+			fmt.Fprintf(os.Stderr, "Unable to serialize test JWT: %s\n", err)
+			return 1
+		}
+
+		return m.Run()
+	}())
 }
--- secure/validator_test.go
+++ secure/validator_test.go
@@ -1,10 +1,33 @@
 package secure
 
 import (
+	"fmt"
+	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 func TestExactMatch(t *testing.T) {
+	assert := assert.New(t)
+
+	const tokenValue = "this value should validate"
+	token, err := ParseAuthorization(fmt.Sprintf("Basic %s", tokenValue))
+	if !assert.NotNil(token) || !assert.Nil(err) {
+		assert.FailNow("ParseAuthorization failed: token=%v, err=%s", token, err)
+	}
+
+	successValidator := ValidateExactMatch(tokenValue)
+	assert.NotNil(successValidator)
+
+	valid, err := successValidator.Validate(token)
+	assert.True(valid)
+	assert.Nil(err)
+
+	failureValidator := ValidateExactMatch("this should not be valid")
+	assert.NotNil(failureValidator)
+
+	valid, err = failureValidator.Validate(token)
+	assert.False(valid)
+	assert.Nil(err)
 }
 
 func TestVerify(t *testing.T) {
