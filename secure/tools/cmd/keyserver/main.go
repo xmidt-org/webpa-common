83e6676dcd6ce9c8a173dbbacf279e0b30394162
John Bass <John_Bass2@cable.comcast.com>
2016-09-14 14:51:47 -0400

    Added some explanatory log messages on startup

--- secure/tools/cmd/keyserver/main.go
+++ secure/tools/cmd/keyserver/main.go
@@ -10,25 +10,36 @@ import (
 	"os"
 )
 
-func addRoutes(issuer string, errorLogger *log.Logger, keyStore *KeyStore, router *mux.Router) {
+type RouteBuilder struct {
+	Issuer      string
+	InfoLogger  *log.Logger
+	ErrorLogger *log.Logger
+	KeyStore    *KeyStore
+}
+
+func (rb RouteBuilder) Build(router *mux.Router) {
 	keyHandler := KeyHandler{
 		BasicHandler{
-			keyStore:    keyStore,
-			errorLogger: errorLogger,
+			keyStore:    rb.KeyStore,
+			errorLogger: rb.ErrorLogger,
 		},
 	}
 
 	keysRouter := router.Methods("GET").Subrouter()
+
 	keysRouter.HandleFunc("/keys", keyHandler.ListKeys)
+	rb.InfoLogger.Println("GET /keys returns a list of the identifiers of available keys")
+
 	keysRouter.HandleFunc(fmt.Sprintf("/keys/{%s}", KeyIDVariableName), keyHandler.GetKey)
+	rb.InfoLogger.Println("GET /keys/{kid} returns the public key associated with the given key identifier.  There is no way to look up the associated private key.")
 
 	issueHandler := IssueHandler{
 		BasicHandler: BasicHandler{
-			keyStore:    keyStore,
-			errorLogger: errorLogger,
+			keyStore:    rb.KeyStore,
+			errorLogger: rb.ErrorLogger,
 		},
 		decoder: schema.NewDecoder(),
-		issuer:  issuer,
+		issuer:  rb.Issuer,
 	}
 
 	issueRouter := router.
@@ -38,10 +49,12 @@ func addRoutes(issuer string, errorLogger *log.Logger, keyStore *KeyStore, route
 
 	issueRouter.Methods("GET").
 		HandlerFunc(issueHandler.SimpleIssue)
+	rb.InfoLogger.Println("GET /jws?kid={kid} generates a JWT signed with the associated private key.  Additional URL parameters are interpreted as reserved claims, e.g. exp")
 
 	issueRouter.Methods("PUT", "POST").
 		Headers("Content-Type", "application/json").
 		HandlerFunc(issueHandler.IssueUsingBody)
+	rb.InfoLogger.Println("PUT/POST /jws generates a JWT signed with the associated private key.  Additional URL parmaeters are interpreted as reserved claims, e.g. exp")
 }
 
 func main() {
@@ -70,7 +83,12 @@ func main() {
 	}
 
 	router := mux.NewRouter()
-	addRoutes(issuer, errorLogger, keyStore, router)
+	RouteBuilder{
+		Issuer:      issuer,
+		ErrorLogger: errorLogger,
+		InfoLogger:  infoLogger,
+		KeyStore:    keyStore,
+	}.Build(router)
 
 	bindAddress := configuration.BindAddress
 	if len(bindAddress) == 0 {
