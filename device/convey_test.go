05a1db8ec0c4d14fb2a562c6ac7b2e53ab8f538a
John Bass <John_Bass2@cable.comcast.com>
2016-11-03 18:11:11 -0700

    Moved convey test data outside tests so it can be shared

--- device/convey_test.go
+++ device/convey_test.go
@@ -7,38 +7,46 @@ import (
 	"testing"
 )
 
+var conveyTestData = []struct {
+	encoded      string
+	decoded      map[string]interface{}
+	valid        bool
+	expectedJSON string
+}{
+	{
+		"this is not valid",
+		nil,
+		false,
+		"",
+	},
+	{
+		"eyJmb28iOiAiYmFyIn0=",
+		map[string]interface{}{"foo": "bar"},
+		true,
+		`{"foo": "bar"}`,
+	},
+	{
+		"eyJkZXZpY2UiOiAiMTIzIiwgImF0dHJpYnV0ZXMiOiB7InZhbHVlIjogMTIzLCAiY29ubmVjdGVkIjogdHJ1ZSwgIm5hbWUiOiAiZm9vYmFyIn19",
+		map[string]interface{}{"device": "123", "attributes": map[string]interface{}{"value": 123, "connected": true, "name": "foobar"}},
+		true,
+		`{"device": "123", "attributes": {"value": 123, "connected": true, "name": "foobar"}}`,
+	},
+}
+
+func TestNewConvey(t *testing.T) {
+}
+
 func TestParseConvey(t *testing.T) {
 	assert := assert.New(t)
-	testData := []struct {
-		value        string
-		expectsError bool
-		expectedJSON string
-	}{
-		{
-			"this is not valid",
-			true,
-			"",
-		},
-		{
-			"eyJmb28iOiAiYmFyIn0=",
-			false,
-			`{"foo": "bar"}`,
-		},
-		{
-			"eyJkZXZpY2UiOiAiMTIzIiwgImF0dHJpYnV0ZXMiOiB7InZhbHVlIjogMTIzLCAiY29ubmVjdGVkIjogdHJ1ZSwgIm5hbWUiOiAiZm9vYmFyIn19",
-			false,
-			`{"device": "123", "attributes": {"value": 123, "connected": true, "name": "foobar"}}`,
-		},
-	}
 
-	for _, record := range testData {
+	for _, record := range conveyTestData {
 		t.Logf("%v", record)
-		convey, err := ParseConvey(record.value)
-		assert.Equal(record.expectsError, convey == nil)
-		assert.Equal(record.expectsError, err != nil)
+		convey, err := ParseConvey(record.encoded)
+		assert.Equal(record.valid, convey != nil)
+		assert.Equal(record.valid, err == nil)
 
 		if convey != nil {
-			assert.Equal(record.value, convey.Encoded())
+			assert.Equal(record.encoded, convey.Encoded())
 			assert.NotEmpty(convey.String())
 			t.Logf(convey.String())
 
