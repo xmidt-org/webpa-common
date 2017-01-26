f0881752b4ecc270f222a87ff52a4f1ea3d97c33
John Bass <John_Bass2@cable.comcast.com>
2017-01-26 12:32:33 -0800

    Parse the endpoint as a net.JoinHostPort formatted string supplying http as the default scheme

--- service/accessor.go
+++ service/accessor.go
@@ -1,10 +1,35 @@
 package service
 
 import (
+	"fmt"
 	"github.com/Comcast/webpa-common/logging"
 	"github.com/billhathaway/consistentHash"
+	"net"
 	"sort"
+	"strings"
 )
+
+// ParseHostPort parses a value of the form returned by net.JoinHostPort and
+// produces a base URL.  If no scheme is present, this function prepends "http://"
+// to the URL.
+//
+// The go.serversets library returns endpoints in this format.  This function is
+// used to turn and endpoint into a valid base URL for a given service.
+func ParseHostPort(value string) (baseURL string, err error) {
+	var host, portString string
+	host, portString, err = net.SplitHostPort(value)
+	if err != nil {
+		return
+	}
+
+	if strings.Contains(host, "://") {
+		baseURL = fmt.Sprintf("%s:%s", host, portString)
+	} else {
+		baseURL = fmt.Sprintf("http://%s:%s", host, portString)
+	}
+
+	return
+}
 
 // Accessor provides access to services based around []byte keys.
 // *consistentHash.ConsistentHash implements this interface.
@@ -14,8 +39,11 @@ type Accessor interface {
 
 // AccessorFactory is a Factory Interface for creating service Accessors.
 type AccessorFactory interface {
-	// New creates an Accessor using a slice of endpoints
-	New([]string) Accessor
+	// New creates an Accessor using a slice of endpoints.  Each endpoint must
+	// be of the form parseable by ParseHostPort.  Invalid endpoints are skipped
+	// with an error log message.  The returned slice of strings is the sorted
+	// list of base URLs added to the Accessor.
+	New([]string) (Accessor, []string)
 }
 
 // NewAccessorFactory uses a set of Options to produce an AccessorFactory
@@ -33,21 +61,24 @@ type consistentHashFactory struct {
 	vnodeCount int
 }
 
-func (f *consistentHashFactory) New(endpoints []string) Accessor {
+func (f *consistentHashFactory) New(endpoints []string) (Accessor, []string) {
 	hash := consistentHash.New()
 	hash.SetVnodeCount(f.vnodeCount)
 
-	if len(endpoints) > 0 {
-		// make a sorted copy so that we add things in a consistent order
-		sorted := make([]string, len(endpoints))
-		copy(sorted, endpoints)
-		sort.Strings(sorted)
-
-		for _, hostAndPort := range sorted {
-			f.logger.Debug("adding %s", hostAndPort)
-			hash.Add(hostAndPort)
+	baseURLs := make([]string, 0, len(endpoints))
+	for _, endpoint := range endpoints {
+		if baseURL, err := ParseHostPort(endpoint); err != nil {
+			f.logger.Error("Skipping bad endpoint: %s", endpoint)
+		} else {
+			baseURLs = append(baseURLs, baseURL)
 		}
 	}
 
-	return hash
+	// sort first, before adding, to give a consistent ordering
+	sort.Strings(baseURLs)
+	for _, baseURL := range baseURLs {
+		hash.Add(baseURL)
+	}
+
+	return hash, baseURLs
 }
--- service/accessor_test.go
+++ service/accessor_test.go
@@ -3,33 +3,43 @@ package service
 import (
 	"github.com/Comcast/webpa-common/logging"
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 	"testing"
 )
 
 func TestNewAccessorFactory(t *testing.T) {
-	assert := assert.New(t)
-	logger := logging.TestLogger(t)
-	testData := []struct {
-		endpoints    []string
-		expectsError bool
-	}{
-		{
-			nil,
-			true,
-		},
-		{
-			[]string{},
-			true,
-		},
-		{
-			[]string{"http://localhost:0101"},
-			false,
-		},
-		{
-			[]string{"https://host1.net:123", "http://host2.com:9293"},
-			false,
-		},
-	}
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+		logger  = logging.TestLogger(t)
+
+		testData = []struct {
+			endpoints        []string
+			expectedBaseURLs []string
+			expectsError     bool
+		}{
+			{
+				endpoints:        nil,
+				expectedBaseURLs: make([]string, 0),
+				expectsError:     true,
+			},
+			{
+				endpoints:        []string{},
+				expectedBaseURLs: make([]string, 0),
+				expectsError:     true,
+			},
+			{
+				endpoints:        []string{"[http://localhost]:7500"},
+				expectedBaseURLs: []string{"http://localhost:7500"},
+				expectsError:     false,
+			},
+			{
+				endpoints:        []string{"[https://host1.net]:123", "[http://host2.com]:9293"},
+				expectedBaseURLs: []string{"http://host2.com:9293", "https://host1.net:123"},
+				expectsError:     false,
+			},
+		}
+	)
 
 	for _, record := range testData {
 		t.Logf("%v", record)
@@ -40,10 +50,9 @@ func TestNewAccessorFactory(t *testing.T) {
 				continue
 			}
 
-			accessor := factory.New(record.endpoints)
-			if !assert.NotNil(accessor) {
-				continue
-			}
+			accessor, actualBaseURLs := factory.New(record.endpoints)
+			require.NotNil(accessor)
+			assert.Equal(record.expectedBaseURLs, actualBaseURLs)
 
 			endpoint, err := accessor.Get([]byte("key"))
 			if record.expectsError {
