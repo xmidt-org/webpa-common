134073010a80f0dcfcf8694f74af1ed535c71f66
John Bass <John_Bass2@cable.comcast.com>
2017-01-27 17:22:15 -0800

    Comments; added a standard subkey

--- device/viper.go
+++ device/viper.go
@@ -5,6 +5,13 @@ import (
 	"github.com/spf13/viper"
 )
 
+const (
+	// DeviceManagerKey is the Viper subkey under which device.Options are typically stored
+	DeviceManagerKey = "deviceManager"
+)
+
+// NewOptions unmarshals a device.Options from a Viper environment.  Listeners
+// must be configured separately.
 func NewOptions(logger logging.Logger, v *viper.Viper) (o *Options, err error) {
 	o = new(Options)
 	if v != nil {
