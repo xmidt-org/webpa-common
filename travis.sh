102cd2a6f07429ce79e1fc3210fa748801e61e19
John Bass <John_Bass2@cable.comcast.com>
2017-01-31 15:36:34 -0800

    Added output to identify which package is being tested; added msgp library

--- travis.sh
+++ travis.sh
@@ -3,8 +3,11 @@
 set -e
 
 for package in $(go list ./... | grep -v vendor); do
+	echo "*********************************************************"
+	echo "** $package"
+	echo "*********************************************************"
 	go test -race -coverprofile=coverage.txt $package
 
 	# this expects the .travis.yml to setup https://codecov.io/bash locally
-	./codecov.sh
+	# ./codecov.sh
 done
