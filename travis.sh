6825a1cd8490cdb22d308f9faf07165d747b5995
John Bass <John_Bass2@cable.comcast.com>
2017-01-31 15:40:57 -0800

    Added the codecov.sh call back in

--- travis.sh
+++ travis.sh
@@ -9,5 +9,5 @@ for package in $(go list ./... | grep -v vendor); do
 	go test -race -coverprofile=coverage.txt $package
 
 	# this expects the .travis.yml to setup https://codecov.io/bash locally
-	# ./codecov.sh
+	./codecov.sh
 done
