c392bf890055cb8a765eb5fc4e4017eed6d75d63
johnabass <johnbass1017@yahoo.com>
2017-08-21 14:53:18 -0700

    ApplyHeaders is a general-purpose decorator for http.Handler that injects static headers into every response

---
+++ httputil/applyHeaders_test.go
@@ -0,0 +1,87 @@
+package httputil
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func testApplyHeadersUnsupported(t *testing.T, h interface{}) {
+	assert.Panics(t, func() { ApplyHeaders(h) })
+}
+
+func testApplyHeaders(t *testing.T, h interface{}, expected http.Header) {
+	const expectedOutput = "testApplyHeaders"
+
+	var (
+		assert = assert.New(t)
+
+		delegate = http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+			response.Write([]byte(expectedOutput))
+		})
+
+		decorator = ApplyHeaders(h)(delegate)
+		response  = httptest.NewRecorder()
+		request   = httptest.NewRequest("GET", "/", nil)
+	)
+
+	decorator.ServeHTTP(response, request)
+	assert.Equal(expectedOutput, response.Body.String())
+
+	for k, _ := range expected {
+		assert.Equal(expected[k], response.HeaderMap[k])
+	}
+}
+
+func TestApplyHeaders(t *testing.T) {
+	t.Run("Unsupported", func(t *testing.T) {
+		testApplyHeadersUnsupported(t, 123)
+		testApplyHeadersUnsupported(t, []string{"foo", "bar"})
+	})
+
+	t.Run("Nil", func(t *testing.T) { testApplyHeaders(t, nil, nil) })
+
+	t.Run("Empty", func(t *testing.T) {
+		testApplyHeaders(t, http.Header{}, nil)
+		testApplyHeaders(t, map[string][]string{}, nil)
+		testApplyHeaders(t, map[string]string{}, nil)
+	})
+
+	testApplyHeaders(
+		t,
+		http.Header{
+			"X-Something":      []string{"value"},
+			"X-Something-Else": []string{"value1", "value2"},
+		},
+		http.Header{
+			"X-Something":      []string{"value"},
+			"X-Something-Else": []string{"value1", "value2"},
+		},
+	)
+
+	testApplyHeaders(
+		t,
+		map[string][]string{
+			"X-Something":      []string{"value"},
+			"X-Something-Else": []string{"value1", "value2"},
+		},
+		http.Header{
+			"X-Something":      []string{"value"},
+			"X-Something-Else": []string{"value1", "value2"},
+		},
+	)
+
+	testApplyHeaders(
+		t,
+		map[string]string{
+			"X-Something":      "value1",
+			"X-Something-Else": "value2",
+		},
+		http.Header{
+			"X-Something":      []string{"value1"},
+			"X-Something-Else": []string{"value2"},
+		},
+	)
+}
