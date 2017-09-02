c5be4c5f2c3991850c3da7a790bb45c316aa93aa
johnabass <johnbass1017@yahoo.com>
2017-09-02 14:59:05 -0700

    Fleshed out the subscription tests

--- service/subscription_test.go
+++ service/subscription_test.go
@@ -5,6 +5,7 @@ import (
 	"testing"
 	"time"
 
+	"github.com/Comcast/webpa-common/logging"
 	"github.com/go-kit/kit/sd"
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/mock"
@@ -20,6 +21,7 @@ func testSubscribeNoDelay(t *testing.T) {
 		deregisterCalled  = make(chan struct{})
 
 		options = &Options{
+			Logger: logging.NewTestLogger(nil, t),
 			After: func(time.Duration) <-chan time.Time {
 				assert.Fail("The after function should not have been called")
 				return nil
@@ -93,6 +95,171 @@ func testSubscribeNoDelay(t *testing.T) {
 	instancer.AssertExpectations(t)
 }
 
+func testSubscribeDelay(t *testing.T) {
+	var (
+		assert    = assert.New(t)
+		instancer = new(mockInstancer)
+		delay     = make(chan time.Time, 1)
+
+		registeredChannel chan<- sd.Event
+		registerCalled    = make(chan struct{})
+		deregisterCalled  = make(chan struct{})
+
+		options = &Options{
+			Logger:      logging.NewTestLogger(nil, t),
+			UpdateDelay: 5 * time.Minute,
+			After: func(d time.Duration) <-chan time.Time {
+				assert.Equal(5*time.Minute, d)
+				return delay
+			},
+		}
+	)
+
+	instancer.On("Register", mock.MatchedBy(func(ch chan<- sd.Event) bool {
+		registeredChannel = ch
+		return true
+	})).Run(func(mock.Arguments) { close(registerCalled) }).Once()
+
+	instancer.On("Deregister", mock.MatchedBy(func(ch chan<- sd.Event) bool {
+		assert.Equal(registeredChannel, ch)
+		return true
+	})).Run(func(mock.Arguments) { close(deregisterCalled) }).Once()
+
+	// start the subscription under test
+	sub := Subscribe(options, instancer)
+	assert.NotEmpty(sub.String())
+	assert.Zero(len(sub.Updates()))
+
+	select {
+	case <-registerCalled:
+		// passing
+	case <-time.After(time.Second):
+		assert.Fail("Instancer.Register was not called")
+	}
+
+	registeredChannel <- sd.Event{Err: errors.New("expected")}
+	assert.Zero(len(sub.Updates()))
+
+	registeredChannel <- sd.Event{Instances: []string{"localhost:8888"}}
+	assert.Zero(len(sub.Updates()))
+
+	delay <- time.Now()
+	select {
+	case accessor := <-sub.Updates():
+		instance, err := accessor.Get([]byte("some key"))
+		assert.Equal("localhost:8888", instance)
+		assert.NoError(err)
+
+	case <-sub.Stopped():
+		assert.Fail("The subscription should not have stopped")
+
+	case <-time.After(time.Second):
+		assert.Fail("No accessor update occurred")
+	}
+
+	registeredChannel <- sd.Event{Instances: []string{"localhost:1234"}}
+	assert.Zero(len(sub.Updates()))
+
+	registeredChannel <- sd.Event{Instances: []string{"localhost:4321"}}
+	assert.Zero(len(sub.Updates()))
+
+	delay <- time.Now()
+	select {
+	case accessor := <-sub.Updates():
+		instance, err := accessor.Get([]byte("some key"))
+		assert.Equal("localhost:4321", instance)
+		assert.NoError(err)
+
+	case <-sub.Stopped():
+		assert.Fail("The subscription should not have stopped")
+
+	case <-time.After(time.Second):
+		assert.Fail("No accessor update occurred")
+	}
+
+	sub.Stop()
+
+	select {
+	case <-deregisterCalled:
+		// passing
+	case <-time.After(time.Second):
+		assert.Fail("Instancer.Deregister was not called")
+	}
+
+	sub.Stop() // idempotency
+	instancer.AssertExpectations(t)
+}
+
+func testSubscribeMonitorPanic(t *testing.T) {
+	var (
+		assert    = assert.New(t)
+		instancer = new(mockInstancer)
+
+		registeredChannel chan<- sd.Event
+		registerCalled    = make(chan struct{})
+		deregisterCalled  = make(chan struct{})
+
+		options = &Options{
+			Logger: logging.NewTestLogger(nil, t),
+			After: func(time.Duration) <-chan time.Time {
+				assert.Fail("The after function should not have been called")
+				return nil
+			},
+			InstancesFilter: func([]string) []string {
+				panic("expected")
+			},
+		}
+	)
+
+	instancer.On("Register", mock.MatchedBy(func(ch chan<- sd.Event) bool {
+		registeredChannel = ch
+		return true
+	})).Run(func(mock.Arguments) { close(registerCalled) }).Once()
+
+	instancer.On("Deregister", mock.MatchedBy(func(ch chan<- sd.Event) bool {
+		assert.Equal(registeredChannel, ch)
+		return true
+	})).Run(func(mock.Arguments) { close(deregisterCalled) }).Once()
+
+	// start the subscription under test
+	sub := Subscribe(options, instancer)
+	assert.NotEmpty(sub.String())
+	assert.Zero(len(sub.Updates()))
+
+	select {
+	case <-registerCalled:
+		// passing
+	case <-time.After(time.Second):
+		assert.Fail("Instancer.Register was not called")
+	}
+
+	registeredChannel <- sd.Event{Err: errors.New("expected")}
+	assert.Zero(len(sub.Updates()))
+
+	// this should cause the panic
+	registeredChannel <- sd.Event{Instances: []string{"localhost:8888"}}
+	assert.Zero(len(sub.Updates()))
+	select {
+	case <-sub.Stopped():
+		// passing
+
+	case <-time.After(time.Second):
+		assert.Fail("The subscription should have stopped itself after a panic")
+	}
+
+	select {
+	case <-deregisterCalled:
+		// passing
+	case <-time.After(time.Second):
+		assert.Fail("Instancer.Deregister was not called")
+	}
+
+	sub.Stop() // idempotency
+	instancer.AssertExpectations(t)
+}
+
 func TestSubscribe(t *testing.T) {
 	t.Run("NoDelay", testSubscribeNoDelay)
+	t.Run("Delay", testSubscribeDelay)
+	t.Run("MonitorPanic", testSubscribeMonitorPanic)
 }
