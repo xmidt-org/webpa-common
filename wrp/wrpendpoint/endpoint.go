595d57c922a0c30ae1307536c802df011b7f8cfd
johnabass <johnbass1017@yahoo.com>
2017-09-11 14:56:20 -0700

    Tests for the endpoint code

--- wrp/wrpendpoint/endpoint.go
+++ wrp/wrpendpoint/endpoint.go
@@ -9,10 +9,14 @@ import (
 
 const DefaultTimeout = 30 * time.Second
 
-// New constructs a go-kit endpoint for the given WRP service
+// New constructs a go-kit endpoint for the given WRP service.  This endpoint enforces
+// the constraint that ctx must be the context associated with the Request.
 func New(s Service) endpoint.Endpoint {
 	return func(ctx context.Context, value interface{}) (interface{}, error) {
-		return s.ServeWRP(value.(Request))
+		request := value.(*request)
+		request.ctx = ctx
+
+		return s.ServeWRP(request)
 	}
 }
 
@@ -30,14 +34,7 @@ func Timeout(timeout time.Duration) endpoint.Middleware {
 				request            = value.(*request)
 			)
 
-			// generally, use WithContext instead of setting the context so that
-			// the Request is immutable.
-			//
-			// However, this is a special case: we know that the request is not in use
-			// by any other goroutine at this point in its lifecycle.  So, this avoids
-			// a needless object allocation.
 			request.ctx = timeoutCtx
-
 			defer cancel()
 			return next(timeoutCtx, request)
 		}
