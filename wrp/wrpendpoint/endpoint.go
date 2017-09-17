ae22fe8a13d022045308d98c543d00aa08e0ab29
johnabass <johnbass1017@yahoo.com>
2017-09-16 18:36:56 -0700

    Removed the Timeout middleware, as it's not WRP-specific

--- wrp/wrpendpoint/endpoint.go
+++ wrp/wrpendpoint/endpoint.go
@@ -2,12 +2,9 @@ package wrpendpoint
 
 import (
 	"context"
-	"time"
 
 	"github.com/go-kit/kit/endpoint"
 )
-
-const DefaultTimeout = 30 * time.Second
 
 // New constructs a go-kit endpoint for the given WRP service.  This endpoint enforces
 // the constraint that ctx must be the context associated with the Request.
@@ -24,21 +21,4 @@ func Wrap(e endpoint.Endpoint) Service {
 		response, err := e(ctx, request)
 		return response.(Response), err
 	})
-}
-
-// Timeout applies the given timeout to all WRP Requests.  The context's cancellation
-// function is always called.
-func Timeout(timeout time.Duration) endpoint.Middleware {
-	if timeout < 1 {
-		timeout = DefaultTimeout
-	}
-
-	return func(next endpoint.Endpoint) endpoint.Endpoint {
-		return func(ctx context.Context, value interface{}) (interface{}, error) {
-			timeoutCtx, cancel := context.WithTimeout(ctx, timeout)
-			defer cancel()
-
-			return next(timeoutCtx, value)
-		}
-	}
 }
--- wrp/wrpendpoint/endpoint_test.go
+++ wrp/wrpendpoint/endpoint_test.go
@@ -3,7 +3,6 @@ package wrpendpoint
 import (
 	"context"
 	"testing"
-	"time"
 
 	"github.com/stretchr/testify/assert"
 )
@@ -68,50 +67,4 @@ func TestWrap(t *testing.T) {
 	assert.Equal(expectedResponse, actualResponse)
 	assert.NoError(err)
 	assert.True(endpointCalled)
-}
-
-func testTimeout(t *testing.T, timeout time.Duration) {
-	var (
-		assert = assert.New(t)
-
-		request Request = &request{
-			note: note{
-				contents: []byte("request"),
-			},
-		}
-
-		expected Response = &response{
-			note: note{
-				contents: []byte("response"),
-			},
-		}
-
-		nextCalled = false
-		next       = func(ctx context.Context, value interface{}) (interface{}, error) {
-			nextCalled = true
-
-			deadline, ok := ctx.Deadline()
-			assert.False(deadline.IsZero())
-			assert.True(ok)
-
-			assert.NotNil(ctx.Done())
-
-			return expected, nil
-		}
-
-		middleware = Timeout(timeout)
-	)
-
-	actual, err := middleware(next)(context.Background(), request)
-	assert.Equal(expected, actual)
-	assert.NoError(err)
-	assert.True(nextCalled)
-}
-
-func TestTimeout(t *testing.T) {
-	for _, timeout := range []time.Duration{-1, 0, 15 * time.Second, 120 * time.Hour} {
-		t.Run(timeout.String(), func(t *testing.T) {
-			testTimeout(t, timeout)
-		})
-	}
 }
