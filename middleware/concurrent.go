723033268fcd67ee5d7099c836db8ea0c54b1bd5
johnabass <johnbass1017@yahoo.com>
2017-09-18 00:18:30 -0700

    Added a Busy middleware, to guard against storms of clients

--- middleware/concurrent.go
+++ middleware/concurrent.go
@@ -10,12 +10,12 @@ import (
 // a semaphore implemented as a buffered channel.  The context is used for cancellation,
 // and if the context is cancelled then timeoutError is returned if it is not nil, ctx.Err() otherwise.
 func Concurrent(concurrency int, timeoutError error) endpoint.Middleware {
-	semaphore := make(chan struct{}, concurrency)
-	for r := 0; r < concurrency; r++ {
-		semaphore <- struct{}{}
-	}
-
 	return func(next endpoint.Endpoint) endpoint.Endpoint {
+		semaphore := make(chan struct{}, concurrency)
+		for r := 0; r < concurrency; r++ {
+			semaphore <- struct{}{}
+		}
+
 		return func(ctx context.Context, value interface{}) (interface{}, error) {
 			select {
 			case <-ctx.Done():
