c27871e3b98d97bbee017384b601d2ae32124b0a
johnabass <johnbass1017@yahoo.com>
2017-09-18 00:50:59 -0700

    Fleshed out the factory function for HTTP fanout endpoints

--- wrp/wrphttp/fanout.go
+++ wrp/wrphttp/fanout.go
@@ -5,22 +5,30 @@ import (
 	"net/url"
 	"time"
 
+	"github.com/Comcast/webpa-common/httperror"
 	"github.com/Comcast/webpa-common/logging"
+	"github.com/Comcast/webpa-common/middleware"
+	"github.com/Comcast/webpa-common/tracing"
+	"github.com/Comcast/webpa-common/wrp"
+	"github.com/go-kit/kit/endpoint"
 	"github.com/go-kit/kit/log"
+	gokithttp "github.com/go-kit/kit/transport/http"
 )
 
 const (
-	DefaultMethod              = "POST"
-	DefaultEndpoint            = "http://localhost:7000/api/v2/device"
-	DefaultTimeout             = 30 * time.Second
-	DefaultMaxIdleConnsPerHost = 20
-	DefaultConcurrency         = 1000
-	DefaultEncoderPoolSize     = 100
-	DefaultDecoderPoolSize     = 100
+	DefaultMethod                            = "POST"
+	DefaultEndpoint                          = "http://localhost:7000/api/v2/device"
+	DefaultMaxIdleConnsPerHost               = 20
+	DefaultFanoutTimeout       time.Duration = 45 * time.Second
+	DefaultClientTimeout       time.Duration = 30 * time.Second
+	DefaultMaxClients          int64         = 10000
+	DefaultConcurrency                       = 1000
+	DefaultEncoderPoolSize                   = 100
+	DefaultDecoderPoolSize                   = 100
 )
 
-// Fanout describes a WRP service with fans out to other HTTP endpoints, using wrp.NewServiceFanout.
-type Fanout struct {
+// FanoutOptions describe the options available for a go-kit HTTP server that does fanout via middleware.Fanout.
+type FanoutOptions struct {
 	// Logger is the go-kit logger to use when creating the service fanout.  If not set, logging.DefaultLogger is used.
 	Logger log.Logger `json:"-"`
 
@@ -33,8 +41,15 @@ type Fanout struct {
 	// Transport is the http.Client transport
 	Transport http.Transport `json:"transport"`
 
-	// Timeout is the http.Client Timeout.  If not set, DefaultTimeout is used.
-	Timeout time.Duration `json:"timeout"`
+	// FanoutTimeout is the timeout for the entire fanout operation.  If not supplied, DefaultFanoutTimeout is used.
+	FanoutTimeout time.Duration `json:"timeout"`
+
+	// ClientTimeout is the http.Client Timeout.  If not set, DefaultClientTimeout is used.
+	ClientTimeout time.Duration `json:"clientTimeout"`
+
+	// MaxClients is the maximum number of concurrent clients that can be using the fanout.  This should be set to
+	// something larger than the Concurrency field.
+	MaxClients int64 `json:"maxClients"`
 
 	// Concurrency is the maximum number of concurrent fanouts allowed.  This is enforced via a Concurrent middleware.
 	// If this is not set, DefaultConcurrency is used.
@@ -45,9 +60,12 @@ type Fanout struct {
 
 	// DecoderPoolSize is the size of the WRP encoder pool.  If not set, DefaultDecoderPoolSize is used.
 	DecoderPoolSize int
+
+	// Middleware is the extra Middleware to append, which can (and often is) empty
+	Middleware []endpoint.Middleware `json:"-"`
 }
 
-func (f *Fanout) logger() log.Logger {
+func (f *FanoutOptions) logger() log.Logger {
 	if f != nil && f.Logger != nil {
 		return f.Logger
 	}
@@ -55,7 +73,7 @@ func (f *Fanout) logger() log.Logger {
 	return logging.DefaultLogger()
 }
 
-func (f *Fanout) method() string {
+func (f *FanoutOptions) method() string {
 	if f != nil && len(f.Method) > 0 {
 		return f.Method
 	}
@@ -63,7 +81,7 @@ func (f *Fanout) method() string {
 	return DefaultMethod
 }
 
-func (f *Fanout) endpoints() []string {
+func (f *FanoutOptions) endpoints() []string {
 	if f != nil && len(f.Endpoints) > 0 {
 		return f.Endpoints
 	}
@@ -71,7 +89,7 @@ func (f *Fanout) endpoints() []string {
 	return []string{DefaultEndpoint}
 }
 
-func (f *Fanout) urls() ([]*url.URL, error) {
+func (f *FanoutOptions) urls() ([]*url.URL, error) {
 	var urls []*url.URL
 	for _, endpoint := range f.endpoints() {
 		url, err := url.Parse(endpoint)
@@ -85,30 +103,45 @@ func (f *Fanout) urls() ([]*url.URL, error) {
 	return urls, nil
 }
 
-func (f *Fanout) transport() *http.Transport {
+func (f *FanoutOptions) transport() *http.Transport {
+	transport := new(http.Transport)
+
 	if f != nil {
-		copyOf := f.Transport
-		if copyOf.MaxIdleConnsPerHost < 1 {
-			copyOf.MaxIdleConnsPerHost = DefaultMaxIdleConnsPerHost
-		}
-
-		return &copyOf
+		*transport = f.Transport
 	}
 
-	return &http.Transport{
-		MaxIdleConnsPerHost: DefaultMaxIdleConnsPerHost,
+	if transport.MaxIdleConnsPerHost < 1 {
+		transport.MaxIdleConnsPerHost = DefaultMaxIdleConnsPerHost
 	}
+
+	return transport
 }
 
-func (f *Fanout) timeout() time.Duration {
-	if f != nil && f.Timeout > 0 {
-		return f.Timeout
+func (f *FanoutOptions) fanoutTimeout() time.Duration {
+	if f != nil && f.FanoutTimeout > 0 {
+		return f.FanoutTimeout
 	}
 
-	return DefaultTimeout
+	return DefaultFanoutTimeout
 }
 
-func (f *Fanout) concurrency() int {
+func (f *FanoutOptions) clientTimeout() time.Duration {
+	if f != nil && f.ClientTimeout > 0 {
+		return f.ClientTimeout
+	}
+
+	return DefaultClientTimeout
+}
+
+func (f *FanoutOptions) maxClients() int64 {
+	if f != nil && f.MaxClients > 0 {
+		return f.MaxClients
+	}
+
+	return DefaultMaxClients
+}
+
+func (f *FanoutOptions) concurrency() int {
 	if f != nil && f.Concurrency > 0 {
 		return f.Concurrency
 	}
@@ -116,7 +149,7 @@ func (f *Fanout) concurrency() int {
 	return DefaultConcurrency
 }
 
-func (f *Fanout) encoderPoolSize() int {
+func (f *FanoutOptions) encoderPoolSize() int {
 	if f != nil && f.EncoderPoolSize > 0 {
 		return f.EncoderPoolSize
 	}
@@ -124,10 +157,71 @@ func (f *Fanout) encoderPoolSize() int {
 	return DefaultEncoderPoolSize
 }
 
-func (f *Fanout) decoderPoolSize() int {
+func (f *FanoutOptions) decoderPoolSize() int {
 	if f != nil && f.DecoderPoolSize > 0 {
 		return f.DecoderPoolSize
 	}
 
 	return DefaultDecoderPoolSize
+}
+
+func (f *FanoutOptions) middleware() []endpoint.Middleware {
+	if f != nil {
+		return f.Middleware
+	}
+
+	return nil
+}
+
+// NewFanoutEndpoint uses the supplied options to produce a go-kit HTTP server endpoint which
+// fans out to the HTTP endpoints specified in the options.  The endpoint returned from this
+// can be used to build one or more go-kit transport/http.Server objects.
+//
+// The FanoutOptions can be nil, in which case a set of defaults is used.
+func NewFanoutEndpoint(o *FanoutOptions) (endpoint.Endpoint, error) {
+	var (
+		encoderPool = wrp.NewEncoderPool(o.encoderPoolSize(), wrp.Msgpack)
+		decoderPool = wrp.NewDecoderPool(o.decoderPoolSize(), wrp.Msgpack)
+
+		httpClient = &http.Client{
+			Transport: o.transport(),
+			Timeout:   o.clientTimeout(),
+		}
+
+		endpoints       = o.endpoints()
+		fanoutEndpoints = make(map[string]endpoint.Endpoint, len(endpoints))
+	)
+
+	for _, e := range endpoints {
+		url, err := url.Parse(e)
+		if err != nil {
+			return nil, err
+		}
+
+		fanoutEndpoints[e] =
+			gokithttp.NewClient(
+				o.method(),
+				url,
+				ClientEncodeRequestBody(encoderPool, http.Header{"Accept": []string{"application/msgpack"}}),
+				ClientDecodeResponseBody(decoderPool),
+				gokithttp.SetClient(httpClient),
+			).Endpoint()
+	}
+
+	var (
+		middlewareChain = append(
+			[]endpoint.Middleware{
+				middleware.Busy(o.maxClients(), &httperror.E{Code: http.StatusServiceUnavailable, Text: "Server Busy"}),
+				middleware.Timeout(o.fanoutTimeout()),
+				middleware.Concurrent(o.concurrency(), &httperror.E{Code: http.StatusTooManyRequests, Text: "Too Many Requests"}),
+			},
+			o.middleware()...,
+		)
+	)
+
+	return endpoint.Chain(
+			middlewareChain[0],
+			middlewareChain[1:]...,
+		)(middleware.Fanout(tracing.NewSpanner(), fanoutEndpoints)),
+		nil
 }
