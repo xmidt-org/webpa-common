248dc38bc0d0d3f1785773878dbdcf685e5aabb5
johnabass <johnbass1017@yahoo.com>
2017-09-17 22:04:01 -0700

    Expanded Spanned and Mergeable; added helper functions for common tasks

--- tracing/spanError_test.go
+++ tracing/spanError_test.go
@@ -11,13 +11,25 @@ func TestSpanError(t *testing.T) {
 	var (
 		assert        = assert.New(t)
 		expectedError = errors.New("expected")
+		spanner       = NewSpanner()
+		testSpans     = []Span{
+			spanner.Start("first")(nil),
+			spanner.Start("second")(errors.New("expected error")),
+			spanner.Start("third")(nil),
+		}
+
+		withSpans = []Span{
+			spanner.Start("fourth")(nil),
+			spanner.Start("fifth")(errors.New("another expected error")),
+		}
 
 		testData = []struct {
 			spanError     SpanError
 			expectedSpans []Span
 		}{
 			{NewSpanError(expectedError), nil},
-			{NewSpanError(expectedError, &span{}), []Span{&span{}}},
+			{NewSpanError(expectedError, testSpans[0]), []Span{testSpans[0]}},
+			{NewSpanError(expectedError, testSpans...), testSpans},
 		}
 	)
 
@@ -27,5 +39,12 @@ func TestSpanError(t *testing.T) {
 		assert.Equal(expectedError, record.spanError.Err())
 		assert.Equal(expectedError.Error(), record.spanError.Error())
 		assert.Equal(record.expectedSpans, record.spanError.Spans())
+
+		assert.True(record.spanError == record.spanError.WithSpans())
+
+		newError := record.spanError.WithSpans(withSpans...).(SpanError)
+		assert.True(record.spanError != newError)
+		assert.Equal(record.spanError.Err(), newError.Err())
+		assert.Equal(withSpans, newError.Spans())
 	}
 }
