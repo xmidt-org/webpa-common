8511a05393b8da8da3f37f204c67e98cb23e7b30
johnabass <johnbass1017@yahoo.com>
2017-09-19 18:07:05 -0700

    Added doc.go; removed an unused errors file

---
+++ wrp/wrphttp/doc.go
@@ -0,0 +1,6 @@
+/*
+Package wrphttp integrates go-kit's transport/http package with the patterns used by WebPA/XMiDT servers.
+Primarily, this package concerns itself with translating between HTTP requests and responses and WRP requests
+and responses.
+*/
+package wrphttp
