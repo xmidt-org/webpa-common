cfd7669651c228c30081677e2b27b8f5c118f92b
johnabass <johnbass1017@yahoo.com>
2017-09-19 18:39:18 -0700

    Added doc.go and an exmplae

--- tracing/spanner_test.go
+++ tracing/spanner_test.go
@@ -2,12 +2,55 @@ package tracing
 
 import (
 	"errors"
+	"fmt"
+	"sync"
 	"testing"
 	"time"
 
 	"github.com/stretchr/testify/assert"
 	"github.com/stretchr/testify/require"
 )
+
+func ExampleSpanner() {
+	var (
+		now      time.Time
+		duration = 100 * time.Millisecond
+
+		// production code can ignore now and since
+		// we do this just to get consistent output
+		spanner = NewSpanner(
+			Now(func() time.Time { return now }),
+			Since(func(time.Time) time.Duration { return duration }),
+		)
+
+		spans     = make(chan Span, 2)
+		firstDone = new(sync.WaitGroup)
+	)
+
+	firstDone.Add(1)
+	go func() {
+		defer firstDone.Done()
+		finisher := spanner.Start("success")
+		// a successful operation happens here ...
+		spans <- finisher(nil)
+	}()
+
+	go func() {
+		defer close(spans)
+		firstDone.Wait()
+		finisher := spanner.Start("failure")
+		// an operation that fails happens here ...
+		spans <- finisher(errors.New("this operation failed"))
+	}()
+
+	for s := range spans {
+		fmt.Println(s.Name(), s.Start(), s.Duration(), s.Error())
+	}
+
+	// Output:
+	// success 0001-01-01 00:00:00 +0000 UTC 100ms <nil>
+	// failure 0001-01-01 00:00:00 +0000 UTC 100ms this operation failed
+}
 
 func TestSpanner(t *testing.T) {
 	var (
