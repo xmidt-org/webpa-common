c63381fc67cdc2c695309aa1d172f6a5110bfe4d
Joel Unzain <joel_unzain@comcast.com>
2017-10-06 15:01:49 -0700

    Include original path in redirect URL

--- service/redirectHandler_test.go
+++ service/redirectHandler_test.go
@@ -90,8 +90,38 @@ func testRedirectHandlerSuccess(t *testing.T) {
 	accessor.AssertExpectations(t)
 }
 
+func testRedirectHandlerSuccessWithPath(t *testing.T) {
+	var (
+		assert = assert.New(t)
+
+		expectedKey         = []byte("asdfqwer")
+		expectedInstance    = "https://ahost123.com:324"
+		expectedRedirectURL = expectedInstance + "/this/awesome/path"
+		keyFunc             = func(*http.Request) ([]byte, error) { return expectedKey, nil }
+		accessor            = new(mockAccessor)
+
+		response = httptest.NewRecorder()
+		request  = httptest.NewRequest("GET", "https://someIrrelevantHost.com/this/awesome/path//", nil)
+
+		handler = RedirectHandler{
+			Logger:       logging.NewTestLogger(nil, t),
+			KeyFunc:      keyFunc,
+			Accessor:     accessor,
+			RedirectCode: http.StatusTemporaryRedirect,
+		}
+	)
+
+	accessor.On("Get", expectedKey).Return(expectedInstance, error(nil)).Once()
+	handler.ServeHTTP(response, request)
+
+	assert.Equal(handler.RedirectCode, response.Code)
+	assert.Equal(expectedRedirectURL, response.HeaderMap.Get("Location"))
+	accessor.AssertExpectations(t)
+}
+
 func TestRedirectHandler(t *testing.T) {
 	t.Run("KeyFuncError", testRedirectHandlerKeyFuncError)
 	t.Run("AccessorError", testRedirectHandlerAccessorError)
 	t.Run("Success", testRedirectHandlerSuccess)
+	t.Run("SuccessPath", testRedirectHandlerSuccessWithPath)
 }
