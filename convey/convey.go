a12ef206056ff00ea26057496ec2f2558ac61c53
johnabass <johnbass1017@yahoo.com>
2017-11-06 12:23:26 -0800

    New Convey API

---
+++ convey/convey.go
@@ -0,0 +1,96 @@
+package convey
+
+import (
+	"bytes"
+	"encoding/base64"
+	"io"
+	"reflect"
+
+	"github.com/ugorji/go/codec"
+)
+
+var (
+	// conveyHandle is the internal package singleton used to parse Convey JSON
+	conveyHandle codec.Handle = &codec.JsonHandle{
+		BasicHandle: codec.BasicHandle{
+			DecodeOptions: codec.DecodeOptions{
+				MapType: reflect.TypeOf(map[string]interface{}(nil)),
+			},
+		},
+		IntegerAsString: 'L',
+	}
+)
+
+// C represents an arbitrary block of JSON which base64-encoded and typically
+// transmitted as an HTTP header.
+type C map[string]interface{}
+
+// Translator provides translation between the on-the-wire representation of a convey map
+// and its runtime representation.  Instances of Translator are safe for concurrent usage.
+type Translator interface {
+	ReadFrom(io.Reader) (C, error)
+	WriteTo(io.Writer, C) error
+}
+
+// translator is the internal Translator implementation
+type translator struct {
+	encoding *base64.Encoding
+}
+
+// NewTranslator produces a Translator which uses the specified base64 encoding.  If
+// the encoding is nil, base64.StdEncoding is used.
+func NewTranslator(encoding *base64.Encoding) Translator {
+	if encoding == nil {
+		encoding = base64.StdEncoding
+	}
+
+	return &translator{
+		encoding: encoding,
+	}
+}
+
+func (t *translator) ReadFrom(source io.Reader) (C, error) {
+	decoder := codec.NewDecoder(
+		base64.NewDecoder(t.encoding, source),
+		conveyHandle,
+	)
+
+	var convey C
+	err := decoder.Decode(&convey)
+	return convey, err
+}
+
+func (t *translator) WriteTo(destination io.Writer, source C) error {
+	encoder := base64.NewEncoder(t.encoding, destination)
+	err := codec.NewEncoder(
+		base64.NewEncoder(t.encoding, destination),
+		conveyHandle,
+	).Encode(source)
+
+	encoder.Close()
+	return err
+}
+
+func ReadString(t Translator, v string) (C, error) {
+	return t.ReadFrom(
+		bytes.NewReader([]byte(v)),
+	)
+}
+
+func ReadBytes(t Translator, v []byte) (C, error) {
+	return t.ReadFrom(
+		bytes.NewReader(v),
+	)
+}
+
+func WriteString(t Translator, v C) (string, error) {
+	var output bytes.Buffer
+	err := t.WriteTo(&output, v)
+	return output.String(), err
+}
+
+func WriteBytes(t Translator, v C) ([]byte, error) {
+	var output bytes.Buffer
+	err := t.WriteTo(&output, v)
+	return output.Bytes(), err
+}
