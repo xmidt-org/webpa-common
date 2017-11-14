d1af3aee6c6815d69501bb4173a135878c76f713
johnabass <johnbass1017@yahoo.com>
2017-11-13 20:16:46 -0800

    fleshed out tests for the fanout functions

--- middleware/fanout/fanouthttp/fanout_test.go
+++ middleware/fanout/fanouthttp/fanout_test.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"net/http"
 	"net/http/httptest"
+	"net/url"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -114,4 +115,176 @@ func TestDecodeFanoutRequest(t *testing.T) {
 	})
 
 	t.Run("CustomDecoderError", testDecodeFanoutRequestCustomDecoderError)
+}
+
+func testEncodeComponentRequestNilEncoder(t *testing.T) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		original      = httptest.NewRequest("PATCH", "/foo/bar", nil)
+		fanoutRequest = &fanoutRequest{
+			original:    original,
+			relativeURL: &url.URL{Path: "/foo/bar"},
+			entity:      "decoded entity",
+		}
+
+		component = httptest.NewRequest("POST", "http://localhost:1234", nil)
+		encoder   = encodeComponentRequest(nil)
+	)
+
+	require.NotNil(encoder)
+	assert.NoError(encoder(context.Background(), component, fanoutRequest))
+	assert.Equal(original.Method, component.Method)
+	assert.Equal("http://localhost:1234/foo/bar", component.URL.String())
+}
+
+func testEncodeComponentRequestCustomEncoder(t *testing.T) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		original      = httptest.NewRequest("PATCH", "/foo/bar", nil)
+		fanoutRequest = &fanoutRequest{
+			original:    original,
+			relativeURL: &url.URL{Path: "/foo/bar"},
+			entity:      "decoded entity",
+		}
+
+		expectedCtx         = context.WithValue(context.Background(), "foo", "bar")
+		expectedComponent   = httptest.NewRequest("POST", "http://localhost:1234", nil)
+		customEncoderCalled = false
+
+		encoder = encodeComponentRequest(
+			func(actualCtx context.Context, actualComponent *http.Request, v interface{}) error {
+				assert.Equal(expectedCtx, actualCtx)
+				assert.Equal(expectedComponent, actualComponent)
+				assert.Equal("decoded entity", v)
+				customEncoderCalled = true
+				return nil
+			},
+		)
+	)
+
+	require.NotNil(encoder)
+	assert.NoError(encoder(expectedCtx, expectedComponent, fanoutRequest))
+	assert.Equal(original.Method, expectedComponent.Method)
+	assert.Equal("http://localhost:1234/foo/bar", expectedComponent.URL.String())
+	assert.True(customEncoderCalled)
+}
+
+func testEncodeComponentRequestCustomEncoderError(t *testing.T) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		original      = httptest.NewRequest("PATCH", "/foo/bar", nil)
+		fanoutRequest = &fanoutRequest{
+			original:    original,
+			relativeURL: &url.URL{Path: "/foo/bar"},
+			entity:      "decoded entity",
+		}
+
+		expectedCtx         = context.WithValue(context.Background(), "foo", "bar")
+		expectedComponent   = httptest.NewRequest("POST", "http://localhost:1234", nil)
+		expectedError       = errors.New("expected")
+		customEncoderCalled = false
+
+		encoder = encodeComponentRequest(
+			func(actualCtx context.Context, actualComponent *http.Request, v interface{}) error {
+				assert.Equal(expectedCtx, actualCtx)
+				assert.Equal(expectedComponent, actualComponent)
+				assert.Equal("decoded entity", v)
+				customEncoderCalled = true
+				return expectedError
+			},
+		)
+	)
+
+	require.NotNil(encoder)
+	assert.Equal(expectedError, encoder(expectedCtx, expectedComponent, fanoutRequest))
+	assert.Equal(original.Method, expectedComponent.Method)
+	assert.Equal("http://localhost:1234/foo/bar", expectedComponent.URL.String())
+	assert.True(customEncoderCalled)
+}
+
+func TestEncodeComponentRequest(t *testing.T) {
+	t.Run("NilEncoder", testEncodeComponentRequestNilEncoder)
+	t.Run("CustomEncoder", testEncodeComponentRequestCustomEncoder)
+	t.Run("CustomEncoderError", testEncodeComponentRequestCustomEncoderError)
+}
+
+func testNewComponentsInvalidURL(t *testing.T) {
+	assert := assert.New(t)
+	for _, bad := range []string{"h\\ttp://localhost", "/foo/bar", "http://comcast.net:8080/test?v=1"} {
+		components, err := NewComponents([]string{bad}, nil, nil)
+		assert.Empty(components)
+		assert.Error(err)
+	}
+}
+
+func testNewComponentsSuccess(t *testing.T, urls ...string) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		components, err = NewComponents(
+			urls,
+			func(context.Context, *http.Request, interface{}) error {
+				return nil
+			},
+			func(context.Context, *http.Response) (interface{}, error) {
+				return nil, nil
+			},
+		)
+	)
+
+	require.Equal(len(urls), len(components))
+	require.NoError(err)
+
+	for _, url := range urls {
+		_, ok := components[url]
+		assert.True(ok)
+	}
+}
+
+func TestNewComponents(t *testing.T) {
+	t.Run("InvalidURL", testNewComponentsInvalidURL)
+	t.Run("Success", func(t *testing.T) {
+		testNewComponentsSuccess(t, "http://something.comcast.net:8080")
+		testNewComponentsSuccess(t, "http://somehost.com", "https://anotherhost.net:1212/foo/bar")
+	})
+}
+
+func TestNewHandler(t *testing.T) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		request  = httptest.NewRequest("GET", "http://localhost/foo/bar", nil)
+		response = httptest.NewRecorder()
+
+		handler = NewHandler(
+			func(ctx context.Context, v interface{}) (interface{}, error) {
+				fanoutRequest := v.(*fanoutRequest)
+				assert.Equal(request, fanoutRequest.original)
+				assert.Equal("/foo/bar", fanoutRequest.relativeURL.String())
+				assert.Equal("decoded entity", fanoutRequest.entity)
+
+				return "response", nil
+			},
+			func(ctx context.Context, original *http.Request) (interface{}, error) {
+				return "decoded entity", nil
+			},
+			func(ctx context.Context, response http.ResponseWriter, v interface{}) error {
+				assert.Equal("response", v)
+				response.Header().Set("X-Processed", "true")
+				return nil
+			},
+		)
+	)
+
+	require.NotNil(handler)
+	handler.ServeHTTP(response, request)
+	assert.Equal(response.Header().Get("X-Processed"), "true")
 }
