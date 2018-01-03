5d5983cdba716e555364b2bc52429f64769f4256
johnabass <johnbass1017@yahoo.com>
2018-01-03 11:24:48 -0800

    Alice-style constructor that emits a configurable set of static headers

---
+++ xhttp/staticHeaders_test.go
@@ -0,0 +1,57 @@
+package xhttp
+
+import (
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func testStaticHeaders(t *testing.T, extra, expected http.Header) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		response = httptest.NewRecorder()
+		request  = httptest.NewRequest("GET", "/", nil)
+
+		decoratedCalled = false
+		next            = http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
+			decoratedCalled = true
+			assert.Equal(len(expected), len(response.Header()))
+			for k, v := range expected {
+				assert.Equal(v, response.Header()[k])
+			}
+		})
+
+		constructor = StaticHeaders(extra)
+	)
+
+	require.NotNil(constructor)
+	decorated := constructor(next)
+
+	decorated.ServeHTTP(response, request)
+	assert.True(decoratedCalled)
+}
+
+func TestStaticHeaders(t *testing.T) {
+	t.Run("Nil", func(t *testing.T) { testStaticHeaders(t, nil, nil) })
+	t.Run("Empty", func(t *testing.T) { testStaticHeaders(t, http.Header{}, nil) })
+	t.Run("Several", func(t *testing.T) {
+		testStaticHeaders(
+			t,
+			http.Header{
+				"Content-Type": {"application/json"},
+				"x-something":  {"value1", "value2"},
+				"eMPtY":        {},
+			},
+			http.Header{
+				"Content-Type": {"application/json"},
+				"X-Something":  {"value1", "value2"},
+				"Empty":        {},
+			},
+		)
+	})
+}
