e7561a9fd7f40d167ae8fbe46bca1adc1220ac77
Joel Unzain <joel_unzain@comcast.com>
2018-01-19 20:09:58 -0800

    Make shared context behavior generic and pass context values as struct

---
+++ contextcommon/doc.go
@@ -0,0 +1,7 @@
+/*
+Package contextcommon provides common context-related functionality.
+
+This was motivated by the similarity between basic context-related methods
+among webpa packages
+*/
+package contextcommon
