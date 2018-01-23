5a46dbc681dbb35b58566cf3b3906348c1ce710f
Joel Unzain <joel_unzain@comcast.com>
2018-01-22 18:27:24 -0800

    Add metrics related to JWT validation

---
+++ secure/metrics_test.go
@@ -0,0 +1,17 @@
+package secure
+
+import (
+	"testing"
+
+	"github.com/Comcast/webpa-common/xmetrics"
+	"github.com/stretchr/testify/assert"
+)
+
+func newTestJWTValidationMeasure() *JWTValidationMeasures {
+	return NewJWTValidationMeasures(xmetrics.MustNewRegistry(nil, Metrics))
+}
+
+func TestSimpleRun(t *testing.T) {
+	assert := assert.New(t)
+	assert.NotNil(newTestJWTValidationMeasure())
+}
