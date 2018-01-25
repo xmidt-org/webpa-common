bcf05191308cacbe14f3c1c5022943db0ebf9acf
njharter <njharter@hotmail.com>
2018-01-25 12:58:21 -0800

    removing used argument in function signature

--- webhook/factory.go
+++ webhook/factory.go
@@ -45,7 +45,7 @@ type Factory struct {
 // This example uses Viper, which I highly recommend.  You could just pass an io.Reader too, and use
 // the encoding/json package.  In any case, allowing the configuration source to be nil makes a lot
 // of things easier on clients, like creating a test Factory for tests in client code.
-func NewFactory(v *viper.Viper, registry *xmetrics.Registry) (f *Factory, err error) {
+func NewFactory(v *viper.Viper) (f *Factory, err error) {
 	f = &Factory{
 		/* put in any system defaults here.  they won't be overridden by Viper unless they are present in external configuration */
 		UndertakerInterval: DEFAULT_UNDERTAKER_INTERVAL,
