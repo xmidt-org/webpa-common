c8a5ca82aa5d48d257f414cd79d22878989a1418
johnabass <johnbass1017@yahoo.com>
2018-03-09 21:55:53 -0800

    pruned unused mock code

--- service/mocks_test.go
+++ service/mocks_test.go
@@ -25,19 +25,3 @@ func (m *mockAccessor) Get(key []byte) (string, error) {
 	arguments := m.Called(key)
 	return arguments.String(0), arguments.Error(1)
 }
-
-type mockSubscription struct {
-	mock.Mock
-}
-
-func (m *mockSubscription) Stopped() <-chan struct{} {
-	return m.Called().Get(0).(<-chan struct{})
-}
-
-func (m *mockSubscription) Stop() {
-	m.Called()
-}
-
-func (m *mockSubscription) Updates() <-chan Accessor {
-	return m.Called().Get(0).(<-chan Accessor)
-}
