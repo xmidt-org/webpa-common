67fb8464a3ca9d15ff0093b1441fb1a824ccbf79
johnabass <johnbass1017@yahoo.com>
2018-03-16 20:37:23 -0700

    Added tests; added a mock service.Environment

--- service/servicemock/mocks.go
+++ service/servicemock/mocks.go
@@ -1,6 +1,7 @@
 package servicemock
 
 import (
+	"github.com/Comcast/webpa-common/service"
 	"github.com/go-kit/kit/sd"
 	"github.com/stretchr/testify/mock"
 )
@@ -20,6 +21,7 @@ func (m *Registrar) Deregister() {
 	m.Called()
 }
 
+// Instancer is a stretchr/testify mocked sd.Instancer
 type Instancer struct {
 	mock.Mock
 }
@@ -36,4 +38,43 @@ func (m *Instancer) Deregister(events chan<- sd.Event) {
 
 func (m *Instancer) Stop() {
 	m.Called()
+}
+
+// Environment is a stretchr/testify mocked service.Environment
+type Environment struct {
+	mock.Mock
+}
+
+var _ service.Environment = (*Environment)(nil)
+
+func (m *Environment) Register() {
+	m.Called()
+}
+
+func (m *Environment) Deregister() {
+	m.Called()
+}
+
+func (m *Environment) Close() error {
+	return m.Called().Error(0)
+}
+
+func (m *Environment) IsRegistered(instance string) bool {
+	return m.Called(instance).Bool(0)
+}
+
+func (m *Environment) DefaultScheme() string {
+	return m.Called().String(0)
+}
+
+func (m *Environment) Instancers() service.Instancers {
+	return m.Called().Get(0).(service.Instancers)
+}
+
+func (m *Environment) AccessorFactory() service.AccessorFactory {
+	return m.Called().Get(0).(service.AccessorFactory)
+}
+
+func (m *Environment) Closed() <-chan struct{} {
+	return m.Called().Get(0).(<-chan struct{})
 }
