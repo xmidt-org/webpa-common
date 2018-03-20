3b9febb547f562f792ae91158560c0bb5c261731
johnabass <johnbass1017@yahoo.com>
2018-03-20 00:42:21 -0700

    Tests for the Redirect response encoder

---
+++ service/servicehttp/redirect_test.go
@@ -0,0 +1,73 @@
+package servicehttp
+
+import (
+	"context"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Comcast/webpa-common/logging"
+	gokithttp "github.com/go-kit/kit/transport/http"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func testRedirectNoRequestURI(t *testing.T, expectedRedirectCode, actualRedirectCode int) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		httpResponse = httptest.NewRecorder()
+	)
+
+	encoder := Redirect(actualRedirectCode)
+	require.NotNil(encoder)
+
+	err := encoder(
+		logging.WithLogger(context.Background(), logging.NewTestLogger(nil, t)),
+		httpResponse,
+		"http://somewhere.com:8080",
+	)
+
+	assert.NoError(err)
+	assert.Equal(expectedRedirectCode, httpResponse.Code)
+	assert.Equal("http://somewhere.com:8080", httpResponse.HeaderMap.Get("Location"))
+}
+
+func testRedirectWithRequestURI(t *testing.T, expectedRedirectCode, actualRedirectCode int) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		httpResponse = httptest.NewRecorder()
+	)
+
+	encoder := Redirect(actualRedirectCode)
+	require.NotNil(encoder)
+
+	err := encoder(
+		context.WithValue(
+			logging.WithLogger(context.Background(), logging.NewTestLogger(nil, t)),
+			gokithttp.ContextKeyRequestURI,
+			"/api/v2/device",
+		),
+		httpResponse,
+		"http://somewhere.com:8080",
+	)
+
+	assert.NoError(err)
+	assert.Equal(expectedRedirectCode, httpResponse.Code)
+	assert.Equal("http://somewhere.com:8080/api/v2/device", httpResponse.HeaderMap.Get("Location"))
+}
+
+func TestRedirect(t *testing.T) {
+	t.Run("NoRequestURI", func(t *testing.T) {
+		testRedirectNoRequestURI(t, http.StatusTemporaryRedirect, 0)
+		testRedirectNoRequestURI(t, http.StatusMovedPermanently, http.StatusMovedPermanently)
+	})
+
+	t.Run("WithRequestURI", func(t *testing.T) {
+		testRedirectWithRequestURI(t, http.StatusTemporaryRedirect, 0)
+		testRedirectWithRequestURI(t, http.StatusMovedPermanently, http.StatusMovedPermanently)
+	})
+}
