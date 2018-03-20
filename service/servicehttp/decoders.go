4ada8b59d4e0c275081edaa0b1165a28f35758da
johnabass <johnbass1017@yahoo.com>
2018-03-20 00:04:35 -0700

    Added an accessor endpoint; added transport/http support

---
+++ service/servicehttp/decoders.go
@@ -0,0 +1,72 @@
+package servicehttp
+
+import (
+	"context"
+	"fmt"
+	"net/http"
+
+	"github.com/Comcast/webpa-common/service"
+	"github.com/Comcast/webpa-common/xhttp"
+	gokithttp "github.com/go-kit/kit/transport/http"
+	"github.com/gorilla/mux"
+)
+
+// KeyFromHeader produces a go-kit decoder which expects an HTTP header to contain the service key.
+func KeyFromHeader(header string, parser service.KeyParser) gokithttp.DecodeRequestFunc {
+	if len(header) == 0 {
+		panic("A header is required")
+	}
+
+	if parser == nil {
+		panic("A parser is required")
+	}
+
+	missingHeader := &xhttp.Error{
+		Code: http.StatusBadRequest,
+		Text: fmt.Sprintf("missing %s header", header),
+	}
+
+	return func(_ context.Context, r *http.Request) (interface{}, error) {
+		v := r.Header.Get(header)
+		if len(v) == 0 {
+			return nil, missingHeader
+		}
+
+		return parser(v)
+	}
+}
+
+// KeyFromPath uses a gorilla/mux path variable as the source for the service Key.
+func KeyFromPath(variable string, parser service.KeyParser) gokithttp.DecodeRequestFunc {
+	if len(variable) == 0 {
+		panic("A variable is required")
+	}
+
+	if parser == nil {
+		panic("A parser is required")
+	}
+
+	noPathVariables := &xhttp.Error{
+		Code: http.StatusInternalServerError,
+		Text: "no path variables found",
+	}
+
+	missingValue := &xhttp.Error{
+		Code: http.StatusBadRequest,
+		Text: fmt.Sprintf("missing path variable %s", variable),
+	}
+
+	return func(_ context.Context, r *http.Request) (interface{}, error) {
+		vars := mux.Vars(r)
+		if len(vars) == 0 {
+			return nil, noPathVariables
+		}
+
+		v, ok := vars[variable]
+		if !ok {
+			return nil, missingValue
+		}
+
+		return parser(v)
+	}
+}
---
+++ service/servicehttp/decoders_test.go
@@ -0,0 +1,210 @@
+package servicehttp
+
+import (
+	"context"
+	"errors"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Comcast/webpa-common/service"
+	"github.com/gorilla/mux"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func testKeyFromHeaderBlankHeader(t *testing.T) {
+	assert := assert.New(t)
+	assert.Panics(func() {
+		KeyFromHeader("", func(string) (service.Key, error) { return nil, nil })
+	})
+}
+
+func testKeyFromHeaderNilParser(t *testing.T) {
+	assert := assert.New(t)
+	assert.Panics(func() {
+		KeyFromHeader("X-Something", nil)
+	})
+}
+
+func testKeyFromHeaderMissingHeader(t *testing.T) {
+	var (
+		assert      = assert.New(t)
+		require     = require.New(t)
+		httpRequest = httptest.NewRequest("GET", "/", nil)
+	)
+
+	decoder := KeyFromHeader("X-Something", func(string) (service.Key, error) {
+		assert.Fail("The parser should not have been called")
+		return nil, nil
+	})
+
+	require.NotNil(decoder)
+
+	key, err := decoder(context.Background(), httpRequest)
+	assert.Error(err)
+	assert.Nil(key)
+}
+
+func testKeyFromHeaderSuccess(t *testing.T) {
+	var (
+		assert      = assert.New(t)
+		require     = require.New(t)
+		httpRequest = httptest.NewRequest("GET", "/", nil)
+
+		expectedHeaderValue = "expected header value"
+		expectedKey         = service.StringKey("expected key")
+	)
+
+	decoder := KeyFromHeader("X-Something", func(v string) (service.Key, error) {
+		assert.Equal(expectedHeaderValue, v)
+		return expectedKey, nil
+	})
+
+	require.NotNil(decoder)
+	httpRequest.Header.Set("X-Something", expectedHeaderValue)
+	actualKey, err := decoder(context.Background(), httpRequest)
+	assert.NoError(err)
+	assert.Equal(expectedKey, actualKey)
+}
+
+func testKeyFromHeaderParserError(t *testing.T) {
+	var (
+		assert      = assert.New(t)
+		require     = require.New(t)
+		httpRequest = httptest.NewRequest("GET", "/", nil)
+
+		expectedHeaderValue = "expected header value"
+		expectedError       = errors.New("expected parser error")
+	)
+
+	decoder := KeyFromHeader("X-Something", func(v string) (service.Key, error) {
+		assert.Equal(expectedHeaderValue, v)
+		return nil, expectedError
+	})
+
+	require.NotNil(decoder)
+	httpRequest.Header.Set("X-Something", expectedHeaderValue)
+	key, actualError := decoder(context.Background(), httpRequest)
+	assert.Equal(expectedError, actualError)
+	assert.Nil(key)
+}
+
+func TestKeyFromHeader(t *testing.T) {
+	t.Run("BlankHeader", testKeyFromHeaderBlankHeader)
+	t.Run("NilParser", testKeyFromHeaderNilParser)
+	t.Run("MissingHeader", testKeyFromHeaderMissingHeader)
+	t.Run("Success", testKeyFromHeaderSuccess)
+	t.Run("ParserError", testKeyFromHeaderParserError)
+}
+
+func testKeyFromPathBlankVariable(t *testing.T) {
+	assert := assert.New(t)
+	assert.Panics(func() {
+		KeyFromPath("", func(string) (service.Key, error) { return nil, nil })
+	})
+}
+
+func testKeyFromPathNilParser(t *testing.T) {
+	assert := assert.New(t)
+	assert.Panics(func() {
+		KeyFromPath("id", nil)
+	})
+}
+
+func testKeyFromPathSuccess(t *testing.T) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		expectedPathValue = "expected path value"
+		expectedKey       = service.StringKey("expected key")
+		httpRequest       = mux.SetURLVars(
+			httptest.NewRequest("GET", "/", nil),
+			map[string]string{"id": expectedPathValue},
+		)
+	)
+
+	decoder := KeyFromPath("id", func(v string) (service.Key, error) {
+		assert.Equal(expectedPathValue, v)
+		return expectedKey, nil
+	})
+
+	require.NotNil(decoder)
+	actualKey, err := decoder(context.Background(), httpRequest)
+	assert.NoError(err)
+	assert.Equal(expectedKey, actualKey)
+}
+
+func testKeyFromPathParserError(t *testing.T) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		expectedPathValue = "expected path value"
+		expectedError     = errors.New("expected parser error")
+		httpRequest       = mux.SetURLVars(
+			httptest.NewRequest("GET", "/", nil),
+			map[string]string{"id": expectedPathValue},
+		)
+	)
+
+	decoder := KeyFromPath("id", func(v string) (service.Key, error) {
+		assert.Equal(expectedPathValue, v)
+		return nil, expectedError
+	})
+
+	require.NotNil(decoder)
+	key, actualError := decoder(context.Background(), httpRequest)
+	assert.Equal(expectedError, actualError)
+	assert.Nil(key)
+}
+
+func testKeyFromPathMissingVars(t *testing.T) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		httpRequest = httptest.NewRequest("GET", "/", nil)
+	)
+
+	decoder := KeyFromPath("id", func(v string) (service.Key, error) {
+		assert.Fail("The parser should not have been called")
+		return nil, nil
+	})
+
+	require.NotNil(decoder)
+	key, err := decoder(context.Background(), httpRequest)
+	assert.Error(err)
+	assert.Nil(key)
+}
+
+func testKeyFromPathMissingVariable(t *testing.T) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		httpRequest = mux.SetURLVars(
+			httptest.NewRequest("GET", "/", nil),
+			map[string]string{"foo": "bar"},
+		)
+	)
+
+	decoder := KeyFromPath("id", func(v string) (service.Key, error) {
+		assert.Fail("The parser should not have been called")
+		return nil, nil
+	})
+
+	require.NotNil(decoder)
+	key, err := decoder(context.Background(), httpRequest)
+	assert.Error(err)
+	assert.Nil(key)
+}
+
+func TestKeyFromPath(t *testing.T) {
+	t.Run("BlankHeader", testKeyFromPathBlankVariable)
+	t.Run("NilParser", testKeyFromPathNilParser)
+	t.Run("Success", testKeyFromPathSuccess)
+	t.Run("ParserError", testKeyFromPathParserError)
+	t.Run("MissingVars", testKeyFromPathMissingVars)
+	t.Run("MissingVariable", testKeyFromPathMissingVariable)
+}
