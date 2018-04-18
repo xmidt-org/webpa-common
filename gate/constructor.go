a7a33a8dddcc525fd5b0755198b85cf3a7010c2b
johnabass <johnbass1017@yahoo.com>
2018-04-17 17:37:52 -0700

    Tests for the NewConstructor logic

--- gate/constructor.go
+++ gate/constructor.go
@@ -11,7 +11,13 @@ func defaultClosedHandler(response http.ResponseWriter, _ *http.Request) {
 // NewConstructor returns an Alice-style constructor which decorates HTTP handlers with gating logic.  If supplied, the closed
 // handler is invoked instead of the decorated handler whenever the gate is closed.  The closed handler may be nil, in which
 // case a default is used that returns http.StatusServiceUnavailable.
+//
+// If g is nil, this function panics.
 func NewConstructor(g Interface, closed http.Handler) func(http.Handler) http.Handler {
+	if g == nil {
+		panic("A gate is required")
+	}
+
 	if closed == nil {
 		closed = http.HandlerFunc(defaultClosedHandler)
 	}
