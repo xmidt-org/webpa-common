682b7bf960afa44ace5283a5ad72202a8bc5c6ea
johnabass <johnbass1017@yahoo.com>
2018-05-11 12:25:08 -0700

    Attempt to wait on each pid so that the build exits with an error if any test breaks

--- test.sh
+++ test.sh
@@ -5,13 +5,18 @@ test_number=1
 coverage_script="$1"
 
 rm -f *.txt
+pids=()
 
 for package in $(go list ./... | grep -v vendor); do
 	go test -race -coverprofile="coverage-$test_number.txt" $package &
+	pids=(${pids[@]} $!)
 	test_number=`expr $test_number + 1`
 done
 
-wait
+for pid in ${pids[@]}
+do
+	wait $pid
+done
 
 if [ -n "$coverage_script" ]; then
 	echo "Uploading test coverage results using script: $coverage_script"
