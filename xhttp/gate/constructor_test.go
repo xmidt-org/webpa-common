6894ab955a4e37aa1ceebcf9f3bb08b9bf3027f5
johnabass <johnbass1017@yahoo.com>
2018-05-11 23:08:35 -0700

    Fleshed out the constructor tests using the new API

--- xhttp/gate/constructor_test.go
+++ xhttp/gate/constructor_test.go
@@ -1,9 +1,12 @@
 package gate
 
 import (
+	"net/http"
+	"net/http/httptest"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
 )
 
 func testNewConstructorNilGate(t *testing.T) {
@@ -13,6 +16,76 @@ func testNewConstructorNilGate(t *testing.T) {
 	})
 }
 
+func testNewConstructorDefault(t *testing.T, c func(http.Handler) http.Handler, g Interface) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		next = http.HandlerFunc(func(response http.ResponseWriter, _ *http.Request) {
+			response.WriteHeader(201)
+		})
+	)
+
+	require.NotNil(c)
+	require.NotNil(g)
+
+	decorated := c(next)
+	require.NotNil(decorated)
+
+	response := httptest.NewRecorder()
+	decorated.ServeHTTP(response, httptest.NewRequest("GET", "/", nil))
+	assert.Equal(201, response.Code)
+
+	g.Lower()
+	response = httptest.NewRecorder()
+	decorated.ServeHTTP(response, httptest.NewRequest("GET", "/", nil))
+	assert.Equal(http.StatusServiceUnavailable, response.Code)
+}
+
+func testNewConstructorCustomClosed(t *testing.T) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		closed = http.HandlerFunc(func(response http.ResponseWriter, _ *http.Request) {
+			response.Header().Set("X-Test", "foobar")
+			response.WriteHeader(599)
+		})
+
+		next = http.HandlerFunc(func(response http.ResponseWriter, _ *http.Request) {
+			response.WriteHeader(201)
+		})
+
+		g = New(Open)
+		c = NewConstructor(g, WithClosedHandler(closed))
+	)
+
+	require.NotNil(c)
+
+	decorated := c(next)
+	require.NotNil(decorated)
+
+	response := httptest.NewRecorder()
+	decorated.ServeHTTP(response, httptest.NewRequest("GET", "/", nil))
+	assert.Equal(201, response.Code)
+	assert.Empty(response.Header())
+
+	g.Lower()
+	response = httptest.NewRecorder()
+	decorated.ServeHTTP(response, httptest.NewRequest("GET", "/", nil))
+	assert.Equal(599, response.Code)
+	assert.Equal("foobar", response.Header().Get("X-Test"))
+}
+
 func TestNewConstructor(t *testing.T) {
 	t.Run("NilGate", testNewConstructorNilGate)
+	t.Run("Default", func(t *testing.T) {
+		g := New(Open)
+		testNewConstructorDefault(t, NewConstructor(g), g)
+
+		g = New(Open)
+		testNewConstructorDefault(t, NewConstructor(g, WithClosedHandler(nil)), g)
+	})
+
+	t.Run("CustomClosed", testNewConstructorCustomClosed)
 }
