49fe3a5f92436abd62902b9e12f5413395ba1157
Miguel Alexandre <miguel.alexandre@parceiros.nos.pt>
2018-05-30 16:48:58 +0100

    Create a new pointer to the registrator in the for loop

--- service/consul/environment_test.go
+++ service/consul/environment_test.go
@@ -132,8 +132,100 @@ func testNewEnvironmentFull(t *testing.T) {
 	ttlUpdater.AssertExpectations(t)
 }
 
+func testNewEnvironmentMultipleRegistrations(t *testing.T) {
+	defer resetClientFactory()
+
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		logger        = logging.NewTestLogger(nil, t)
+		clientFactory = prepareMockClientFactory()
+		client        = new(mockClient)
+		ttlUpdater    = new(mockTTLUpdater)
+
+		co = Options{
+			Client: &api.Config{
+				Address: "localhost:8500",
+				Scheme:  "https",
+			},
+			Registrations: []api.AgentServiceRegistration{
+				api.AgentServiceRegistration{
+					ID:      "service1",
+					Address: "grubly.com",
+					Port:    1111,
+				},
+				api.AgentServiceRegistration{
+					ID:      "service1",
+					Address: "grubly.local",
+					Port:    1111,
+				},
+			},
+			Watches: []Watch{
+				Watch{
+					Service:     "foobar",
+					Tags:        []string{"tag1"},
+					PassingOnly: true,
+				},
+				Watch{
+					Service:     "foobar",
+					Tags:        []string{"tag1"},
+					PassingOnly: true,
+				}, // duplicates should be ignored
+			},
+		}
+	)
+
+	clientFactory.On("NewClient", mock.MatchedBy(func(*api.Client) bool { return true })).Return(client, ttlUpdater).Once()
+
+	client.On("Service",
+		"foobar",
+		"tag1",
+		true,
+		mock.MatchedBy(func(qo *api.QueryOptions) bool { return qo != nil }),
+	).Return([]*api.ServiceEntry{}, new(api.QueryMeta), error(nil))
+
+	client.On("Register",
+		mock.MatchedBy(func(r *api.AgentServiceRegistration) bool {
+			return r.Address == "grubly.com" && r.Port == 1111
+		}),
+	).Return(error(nil)).Once()
+
+	client.On("Register",
+		mock.MatchedBy(func(r *api.AgentServiceRegistration) bool {
+			return r.Address == "grubly.local" && r.Port == 1111
+		}),
+	).Return(error(nil)).Once()
+
+	client.On("Deregister",
+		mock.MatchedBy(func(r *api.AgentServiceRegistration) bool {
+			return r.Address == "grubly.com" && r.Port == 1111
+		}),
+	).Return(error(nil)).Twice()
+
+	client.On("Deregister",
+		mock.MatchedBy(func(r *api.AgentServiceRegistration) bool {
+			return r.Address == "grubly.local" && r.Port == 1111
+		}),
+	).Return(error(nil)).Twice()
+
+	e, err := NewEnvironment(logger, "", co)
+	require.NoError(err)
+	require.NotNil(e)
+
+	e.Register()
+	e.Deregister()
+
+	assert.NoError(e.Close())
+
+	clientFactory.AssertExpectations(t)
+	client.AssertExpectations(t)
+	ttlUpdater.AssertExpectations(t)
+}
+
 func TestNewEnvironment(t *testing.T) {
 	t.Run("Empty", testNewEnvironmentEmpty)
 	t.Run("ClientError", testNewEnvironmentClientError)
 	t.Run("Full", testNewEnvironmentFull)
+	t.Run("MultipleRegistrations", testNewEnvironmentMultipleRegistrations)
 }
