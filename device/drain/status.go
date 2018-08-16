69af91065fc68977e7e0a641dd98beec26583abe
johnabass <johnbass1017@yahoo.com>
2018-08-16 16:18:43 -0700

    Tests for the status handler; nicer serialization for times

--- device/drain/status.go
+++ device/drain/status.go
@@ -15,15 +15,32 @@ type Status struct {
 func (s *Status) ServeHTTP(response http.ResponseWriter, request *http.Request) {
 	var (
 		active, job, progress = s.Drainer.Status()
-		message, err          = json.Marshal(
-			map[string]interface{}{
-				"active":   active,
-				"job":      job,
-				"progress": progress,
-			},
-		)
+
+		jobMap = map[string]interface{}{
+			"count": job.Count,
+		}
+
+		messageMap = map[string]interface{}{
+			"active":   active,
+			"progress": progress,
+		}
 	)
 
+	// some specialness with the job to make it more human friendly
+	if job.Percent > 0 {
+		jobMap["percent"] = job.Percent
+	}
+
+	if job.Rate > 0 {
+		jobMap["rate"] = job.Rate
+	}
+
+	if job.Tick > 0 {
+		jobMap["tick"] = job.Tick.String()
+	}
+
+	messageMap["job"] = jobMap
+	message, err := json.Marshal(messageMap)
 	if err != nil {
 		xhttp.WriteError(response, http.StatusInternalServerError, err)
 		return
---
+++ device/drain/status_test.go
@@ -0,0 +1,65 @@
+package drain
+
+import (
+	"fmt"
+	"net/http"
+	"net/http/httptest"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func testStatus(t *testing.T, active bool, j Job, p Progress, expectedJSON string) {
+	var (
+		assert = assert.New(t)
+
+		d      = new(mockDrainer)
+		status = Status{d}
+
+		response = httptest.NewRecorder()
+		request  = httptest.NewRequest("GET", "/", nil)
+	)
+
+	d.On("Status").Return(active, j, p).Once()
+	status.ServeHTTP(response, request)
+	assert.Equal(http.StatusOK, response.Code)
+	assert.JSONEq(expectedJSON, response.Body.String())
+	d.AssertExpectations(t)
+}
+
+func TestStatus(t *testing.T) {
+	var (
+		zeroTime = time.Time{}.Format(time.RFC3339Nano)
+		now      = time.Now()
+
+		testData = []struct {
+			active       bool
+			j            Job
+			p            Progress
+			expectedJSON string
+		}{
+			{
+				// when no job has been run since the server started:
+				false,
+				Job{},
+				Progress{},
+				fmt.Sprintf(`{"active": false, "job": {"count": 0}, "progress": {"visited": 0, "drained": 0, "started": "%s"}}`, zeroTime),
+			},
+
+			{
+				true,
+				Job{Count: 67283, Percent: 97, Rate: 127, Tick: 17 * time.Second},
+				Progress{Visited: 12, Drained: 4, Started: now, Finished: &now},
+				fmt.Sprintf(`{"active": true, "job": {"count": 67283, "percent": 97, "rate": 127, "tick": "17s"}, "progress": {"visited": 12, "drained": 4, "started": "%s", "finished": "%s"}}`, now.Format(time.RFC3339Nano), now.Format(time.RFC3339Nano)),
+			},
+		}
+	)
+
+	for i, record := range testData {
+		t.Run(strconv.Itoa(i), func(t *testing.T) {
+			testStatus(t, record.active, record.j, record.p, record.expectedJSON)
+		})
+	}
+}
