23a9cf3fe4273f368bdfbb0a2c3541421c333f8c
johnabass <johnbass1017@yahoo.com>
2018-08-25 11:22:27 -0700

    Created a function type for the strategies

--- xhttp/xcontext/populate.go
+++ xhttp/xcontext/populate.go
@@ -6,13 +6,16 @@ import (
 	"time"
 )
 
+// ContextFunc is a strategy for appending information to the context within an HTTP handler.
+type ContextFunc func(context.Context, *http.Request) context.Context
+
 // Populate accepts any number of go-kit request functions and returns an Alice-style constructor that
 // uses the request functions to build a context.  The resulting context is then assocated with the request
 // prior to the next http.Handler being invoked.
 //
 // This function mimics the behavior of go-kit's transport/http package without requiring and endpoint with
 // encoding and decoding.
-func Populate(timeout time.Duration, rf ...func(context.Context, *http.Request) context.Context) func(http.Handler) http.Handler {
+func Populate(timeout time.Duration, rf ...ContextFunc) func(http.Handler) http.Handler {
 	if timeout > 0 || len(rf) > 0 {
 		return func(next http.Handler) http.Handler {
 			return http.HandlerFunc(func(response http.ResponseWriter, request *http.Request) {
