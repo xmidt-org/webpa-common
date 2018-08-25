b06e7842f22ff0ace2461a036c96faabd33b3e02
johnabass <johnbass1017@yahoo.com>
2018-08-25 12:19:26 -0700

    Context strategy for injecting an ErrorEncoder

---
+++ xhttp/xcontext/setErrorEncoder.go
@@ -0,0 +1,16 @@
+package xcontext
+
+import (
+	"context"
+	"net/http"
+
+	"github.com/Comcast/webpa-common/xhttp"
+	gokithttp "github.com/go-kit/kit/transport/http"
+)
+
+// SetErrorEncoder is a ContextFunc strategy that injects a go-kit ErrorEncoder into the HTTP request context.
+func SetErrorEncoder(ee gokithttp.ErrorEncoder) ContextFunc {
+	return func(ctx context.Context, _ *http.Request) context.Context {
+		return xhttp.WithErrorEncoder(ctx, ee)
+	}
+}
---
+++ xhttp/xcontext/setErrorEncoder_test.go
@@ -0,0 +1,48 @@
+package xcontext
+
+import (
+	"context"
+	"errors"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Comcast/webpa-common/xhttp"
+	gokithttp "github.com/go-kit/kit/transport/http"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func testSetErrorEncoderDefault(t *testing.T) {
+	var (
+		assert = assert.New(t)
+		ctx    = SetErrorEncoder(nil)(context.Background(), httptest.NewRequest("GET", "/", nil))
+	)
+
+	assert.NotNil(xhttp.GetErrorEncoder(ctx))
+}
+
+func testSetErrorEncoderCustom(t *testing.T) {
+	var (
+		assert  = assert.New(t)
+		require = require.New(t)
+
+		expectedCalled                        = false
+		expected       gokithttp.ErrorEncoder = func(context.Context, error, http.ResponseWriter) {
+			expectedCalled = true
+		}
+
+		actual = xhttp.GetErrorEncoder(
+			SetErrorEncoder(expected)(context.Background(), httptest.NewRequest("GET", "/", nil)),
+		)
+	)
+
+	require.NotNil(actual)
+	actual(context.Background(), errors.New("expected"), httptest.NewRecorder())
+	assert.True(expectedCalled)
+}
+
+func TestSetErrorEncoder(t *testing.T) {
+	t.Run("Default", testSetErrorEncoderDefault)
+	t.Run("Custom", testSetErrorEncoderCustom)
+}
