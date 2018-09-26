83664c95bdd9b9d18cd83bb43f9c1bc554885c1a
Jack Murdock <jack_murdock@comcast.com>
2018-09-26 13:49:30 -0700

    using reflection to get the first error

--- xerrors/xerrors_test.go
+++ xerrors/xerrors_test.go
@@ -11,15 +11,133 @@ import (
 	"time"
 )
 
-func TestGetXErrorBasic(t *testing.T) {
+type subError struct {
+	err error
+	str string
+}
+
+func (err subError) Error() string {
+	return fmt.Sprintf("%s(%s)", err.str, err.err.Error())
+}
+
+type subErrorPtr struct {
+	err *error
+	str string
+}
+
+func (err subErrorPtr) Error() string {
+	return fmt.Sprintf("%s(%s)", err.str, (*err.err).Error())
+}
+
+func TestGetError_Int(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(nil, getError(5))
+
+}
+func TestGetError_String(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(nil, getError("hi"))
+}
+func TestGetError_Nil(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Equal(nil, getError(nil))
+}
+
+func TestGetError_Error(t *testing.T) {
+	assert := assert.New(t)
+
+	expected := errors.New("hi")
+	assert.Equal(expected, getError(expected))
+}
+func TestGetError_ErrorPTR(t *testing.T) {
+	assert := assert.New(t)
+
+	expected := errors.New("hi")
+	assert.Equal(expected, getError(&expected))
+}
+
+func TestGetError_ErrorComplex(t *testing.T) {
+	assert := assert.New(t)
+	type testA struct {
+		error
+		str string
+	}
+	expected := testA{errors.New("hi"), "bye"}
+	assert.Equal(expected, getError(expected))
+}
+
+func TestGetError_ErrorComplexWithPointer(t *testing.T) {
+	assert := assert.New(t)
+	type testA struct {
+		error
+		str string
+	}
+	expected := testA{errors.New("hi"), "bye"}
+	assert.Equal(expected, getError(&expected))
+}
+
+func TestGetError_NoError(t *testing.T) {
+	assert := assert.New(t)
+	type testA struct {
+		str string
+	}
+	expected := testA{"bye"}
+	assert.Nil(getError(&expected))
+}
+
+func TestFirstCause_CustomSubError(t *testing.T) {
+	assert := assert.New(t)
+	type testA struct {
+		error
+		str string
+	}
+	exectedErr := errors.New("testA")
+	test := testA{exectedErr, "cool"}
+	assert.Equal(exectedErr, FirstCause(subError{test, "neat"}))
+}
+
+func TestFirstCause_Nil(t *testing.T) {
+	assert := assert.New(t)
+	
+	assert.Nil(FirstCause(nil))
+}
+
+func TestFirstCause_ChainSubError(t *testing.T) {
+	assert := assert.New(t)
+
+	exectedErr := errors.New("expected error")
+	test := subError{
+		subError{
+			subError{
+				subErrorPtr{&exectedErr, "cool"},
+				"c",
+			},
+			"b",
+		},
+		"a",
+	}
+	assert.Equal(exectedErr, FirstCause(subError{test, "root"}))
+	t.Logf("%s <-> %s", exectedErr.Error(), test.Error())
+}
+
+func TestGetError_SubError(t *testing.T) {
+	assert := assert.New(t)
+
+	expected := subError{errors.New("hi"), "bye"}
+	assert.Equal(expected, getError(&expected))
+}
+
+func TestFirstCause_Basic(t *testing.T) {
 	assert := assert.New(t)
 
 	err := errors.New("my bad")
-	expected := &XError{ErrorBucket{UnknownError: {}}, err}
-	assert.Equal(expected, GetXError(err))
+	assert.Equal(err, FirstCause(err))
 }
 
-func testXErrorHandler(t *testing.T, serverSleep time.Duration, contextDeadline time.Duration, timeout time.Duration, useDefer bool, expectedErrorSet ErrorBucket) *XError {
+func testFirstCauseHTTPHandler(t *testing.T, serverSleep time.Duration, contextDeadline time.Duration, timeout time.Duration, useDefer bool) {
 	assert := assert.New(t)
 
 	ts := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
@@ -47,44 +165,30 @@ func testXErrorHandler(t *testing.T, serverSleep time.Duration, contextDeadline 
 		assert.FailNow("clientErr can not be nil to continue")
 	}
 
-	xerr := GetXError(clientErr)
-	assert.Equal(expectedErrorSet, xerr.ErrorBucket)
+	xerr := FirstCause(clientErr)
+	assert.Error(xerr)
 
 	t.Logf("%#v\n", clientErr)
 	t.Log(xerr)
-	return xerr
 }
 
-func TestGetXError(t *testing.T) {
+func TestFirstCause_HTTP(t *testing.T) {
 
 	testData := []struct {
-		name             string
-		serverSleep      time.Duration
-		contextDeadline  time.Duration
-		timeout          time.Duration
-		useDefer         bool
-		expectedErrorSet ErrorBucket
+		name            string
+		serverSleep     time.Duration
+		contextDeadline time.Duration
+		timeout         time.Duration
+		useDefer        bool
 	}{
-		{"client-timeout", time.Second, 5 * time.Millisecond, time.Millisecond, true, ErrorBucket{RequestCanceledError: {}, TemporaryError: {}, TimeoutError: {}, URLError: {}}},
-		{"context-cancel", time.Nanosecond, 5 * time.Millisecond, time.Millisecond, false, ErrorBucket{ContextCanceledError: {}, URLError: {}}},
-		{"context-timeout", time.Second, time.Millisecond, 5 * time.Millisecond, true, ErrorBucket{ContextDeadlineExceededError: {}, TemporaryError: {}, TimeoutError: {}, URLError: {}}},
+		{"client-timeout", time.Second, 5 * time.Millisecond, time.Millisecond, true},
+		{"context-cancel", time.Nanosecond, 5 * time.Millisecond, time.Millisecond, false},
+		{"context-timeout", time.Second, time.Millisecond, 5 * time.Millisecond, true},
 	}
 
 	for _, record := range testData {
 		t.Run(fmt.Sprintf("handle/%s", record.name), func(t *testing.T) {
-			xerr := testXErrorHandler(t, record.serverSleep, record.contextDeadline, record.timeout, record.useDefer, record.expectedErrorSet)
-			count := 0
-			if xerr.IsClientTimeout(){
-				count ++
-			}
-			if xerr.IsContextCanceled(){
-				count ++
-			}
-			if xerr.IsContextTimeout(){
-				count ++
-			}
-			assert.Equal(t, 1, count, "error can't be context timeout context canceled and client timeout")
+			testFirstCauseHTTPHandler(t, record.serverSleep, record.contextDeadline, record.timeout, record.useDefer)
 		})
 	}
-
 }
