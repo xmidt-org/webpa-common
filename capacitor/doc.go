e8fa25ea020374ce820581746d59c268c4823744
johnabass <johnbass1017@yahoo.com>
2018-10-16 14:41:06 -0700

    Initial version of a capacitor

---
+++ capacitor/doc.go
@@ -0,0 +1,5 @@
+/*
+ Package capacitor provides a configurable delay for a series of function calls.  A capacitor is discharged
+ when it is time to actually invoke the target function.
+*/
+package capacitor
