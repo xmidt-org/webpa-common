9e6907eaf8374cd422670153ce22296f6d81d730
Jack Murdock <kcajmagic@gmail.com>
2018-11-29 08:36:47 -0800

    removed exportation of Handler and removed custom logger

--- bookkeeping/bookkeeper_test.go
+++ bookkeeping/bookkeeper_test.go
@@ -77,37 +77,3 @@ func TestBookkeeper(t *testing.T) {
 
 	}
 }
-
-func TestBookkeeperLogger(t *testing.T) {
-	var (
-		assert           = assert.New(t)
-		require          = require.New(t)
-		transactorCalled = false
-		logger           = logging.NewCaptureLogger()
-		bookkeeper       = New(WithRequests(Path), WithResponses(Code), WithLogger(logger))
-	)
-
-	require.NotNil(bookkeeper)
-	req := httptest.NewRequest("GET", "/", nil)
-	rr := httptest.NewRecorder()
-
-	handler := http.HandlerFunc(func(writer http.ResponseWriter, request *http.Request) {
-		transactorCalled = true
-		writer.Write([]byte("payload"))
-		writer.WriteHeader(200)
-	})
-
-	bookkeeper(handler).ServeHTTP(rr, req)
-
-	assert.True(transactorCalled)
-
-	select {
-	case result := <-logger.Output():
-		assert.Len(result, 6)
-		assert.Equal(req.URL.Path, result["path"])
-		assert.Equal(200, result["code"])
-	default:
-		assert.Fail("CaptureLogger must capture something")
-
-	}
-}
