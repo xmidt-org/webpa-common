a96006fbd78eb06971a8f7b349b98fefbbb72f06
Joel Unzain <joel_unzain@comcast.com>
2019-11-18 11:36:59 -0800

    Mark packages to be deprecated

--- secure/doc.go
+++ secure/doc.go
@@ -1,4 +1,8 @@
 /*
 Package secure contains the security types common to WebPA applications
+
+Deprecated: secure is no longer planned to be used by future WebPA/XMiDT services
+
+This package is frozen and no new functionality will be addded.
 */
 package secure
--- server/doc.go
+++ server/doc.go
@@ -1,4 +1,8 @@
 /*
 Package server provides the standard approach to executing servers for WebPA
+
+Deprecated: server is no longer planned to be used by future WebPA/XMiDT services
+
+This package is frozen and no new functionality will be addded.
 */
 package server
