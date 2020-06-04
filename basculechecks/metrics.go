f07406b4eb638017530a10758b18d08f98cc4109
Jack Murdock <kcajmagic@gmail.com>
2020-06-04 09:40:00 -0700

    add provide method for uber.fx style (#488)
    * adding uberfx to basculemetrics
    
    * add provider method
    
    * fix histogram provider

--- basculechecks/metrics.go
+++ basculechecks/metrics.go
@@ -3,10 +3,14 @@ package basculechecks
 import (
 	"github.com/go-kit/kit/metrics"
 	"github.com/go-kit/kit/metrics/provider"
+	"github.com/prometheus/client_golang/prometheus"
+	themisXmetrics "github.com/xmidt-org/themis/xmetrics"
 	"github.com/xmidt-org/webpa-common/xmetrics"
+
+	"go.uber.org/fx"
 )
 
-//Names for our metrics
+// Names for our metrics
 const (
 	AuthCapabilityCheckOutcome = "auth_capability_check"
 )
@@ -32,7 +36,7 @@ const (
 	NoCapabilitiesMatch      = "no_capabilities_match"
 )
 
-//Metrics returns the Metrics relevant to this package
+// Metrics returns the Metrics relevant to this package
 func Metrics() []xmetrics.Metric {
 	return []xmetrics.Metric{
 		xmetrics.Metric{
@@ -44,12 +48,24 @@ func Metrics() []xmetrics.Metric {
 	}
 }
 
-//AuthCapabilityCheckMeasures describes the defined metrics that will be used by clients
-type AuthCapabilityCheckMeasures struct {
-	CapabilityCheckOutcome metrics.Counter
+func ProvideMetrics() fx.Option {
+	return fx.Provide(
+		themisXmetrics.ProvideCounter(prometheus.CounterOpts{
+			Name:        AuthCapabilityCheckOutcome,
+			Help:        "Counter for the capability checker, providing outcome information by client, partner, and endpoint",
+			ConstLabels: nil,
+		}, OutcomeLabel, ReasonLabel, ClientIDLabel, PartnerIDLabel, EndpointLabel),
+	)
 }
 
-//NewAuthCapabilityCheckMeasures realizes desired metrics
+// AuthCapabilityCheckMeasures describes the defined metrics that will be used by clients
+type AuthCapabilityCheckMeasures struct {
+	fx.In
+
+	CapabilityCheckOutcome metrics.Counter `name:"auth_capability_check"`
+}
+
+// NewAuthCapabilityCheckMeasures realizes desired metrics
 func NewAuthCapabilityCheckMeasures(p provider.Provider) *AuthCapabilityCheckMeasures {
 	return &AuthCapabilityCheckMeasures{
 		CapabilityCheckOutcome: p.NewCounter(AuthCapabilityCheckOutcome),
--- basculemetrics/metrics.go
+++ basculemetrics/metrics.go
@@ -3,10 +3,13 @@ package basculemetrics
 import (
 	"github.com/go-kit/kit/metrics"
 	gokitprometheus "github.com/go-kit/kit/metrics/prometheus"
+	"github.com/prometheus/client_golang/prometheus"
+	themisXmetrics "github.com/xmidt-org/themis/xmetrics"
 	"github.com/xmidt-org/webpa-common/xmetrics"
+	"go.uber.org/fx"
 )
 
-//Names for our metrics
+// Names for our metrics
 const (
 	AuthValidationOutcome = "auth_validation"
 	NBFHistogram          = "auth_from_nbf_seconds"
@@ -18,7 +21,7 @@ const (
 	OutcomeLabel = "outcome"
 )
 
-//Metrics returns the Metrics relevant to this package
+// Metrics returns the Metrics relevant to this package
 func Metrics() []xmetrics.Metric {
 	return []xmetrics.Metric{
 		xmetrics.Metric{
@@ -42,14 +45,36 @@ func Metrics() []xmetrics.Metric {
 	}
 }
 
-//AuthValidationMeasures describes the defined metrics that will be used by clients
-type AuthValidationMeasures struct {
-	NBFHistogram      *gokitprometheus.Histogram
-	ExpHistogram      *gokitprometheus.Histogram
-	ValidationOutcome metrics.Counter
+func ProvideMetrics() fx.Option {
+	return fx.Provide(
+		themisXmetrics.ProvideCounter(prometheus.CounterOpts{
+			Name:        AuthValidationOutcome,
+			Help:        "Counter for the capability checker, providing outcome information by client, partner, and endpoint",
+			ConstLabels: nil,
+		}, OutcomeLabel),
+		themisXmetrics.ProvideHistogram(prometheus.HistogramOpts{
+			Name:    NBFHistogram,
+			Help:    "Difference (in seconds) between time of JWT validation and nbf (including leeway)",
+			Buckets: []float64{-61, -11, -2, -1, 0, 9, 60}, // defines the upper inclusive (<=) bounds
+		}),
+		themisXmetrics.ProvideHistogram(prometheus.HistogramOpts{
+			Name:    EXPHistogram,
+			Help:    "Difference (in seconds) between time of JWT validation and exp (including leeway)",
+			Buckets: []float64{-61, -11, -2, -1, 0, 9, 60},
+		}),
+	)
 }
 
-//NewAuthValidationMeasures realizes desired metrics
+// AuthValidationMeasures describes the defined metrics that will be used by clients
+type AuthValidationMeasures struct {
+	fx.In
+
+	NBFHistogram      metrics.Histogram `name:"auth_from_nbf_seconds"`
+	ExpHistogram      metrics.Histogram `name:"auth_from_exp_seconds"`
+	ValidationOutcome metrics.Counter   `name:"auth_validation"`
+}
+
+// NewAuthValidationMeasures realizes desired metrics
 func NewAuthValidationMeasures(r xmetrics.Registry) *AuthValidationMeasures {
 	return &AuthValidationMeasures{
 		NBFHistogram:      gokitprometheus.NewHistogram(r.NewHistogramVec(NBFHistogram)),
