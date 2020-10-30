a005079f9c5a90c29188717e1a0d92414ebd4aa6
Joel Unzain <joe94@users.noreply.github.com>
2020-10-30 12:02:24 -0700

    New webhook service (#528)
    * initial skeleton for the new webhook service
    
    * some renaming and start of service implementation
    
    * Add main watcher logic
    
    * add poll count changes
    
    * initial transport functions
    
    * create basic version of http handler
    
    * consul ambiguous import error workaround
    
    * ambiguous import error fixed in go1.15.3
    
    * export handlers and change initialize signature
    
    * use json library for both encode/decode
    
    * use empty slice for correct json marshalling
    
    * switch back to argus having a listener
    
    * odd error with ambiguous import was back

---
+++ xwebhook/webhook.go
@@ -0,0 +1,47 @@
+package xwebhook
+
+import (
+	"time"
+)
+
+// Webhook contains all the information needed to serve events to webhook listeners.
+type Webhook struct {
+	// Address is the subscription request origin HTTP Address.
+	Address string `json:"registered_from_address"`
+
+	// Config contains data to inform how events are delivered.
+	Config struct {
+		// URL is the HTTP URL to deliver messages to.
+		URL string `json:"url"`
+
+		// ContentType is content type value to set WRP messages to (unless already specified in the WRP).
+		ContentType string `json:"content_type"`
+
+		// Secret is the string value for the SHA1 HMAC.
+		// (Optional, set to "" to disable behavior).
+		Secret string `json:"secret,omitempty"`
+
+		// AlternativeURLs is a list of explicit URLs that should be round robin through on failure cases to the main URL.
+		AlternativeURLs []string `json:"alt_urls,omitempty"`
+	} `json:"config"`
+
+	// FailureURL is the URL used to notify subscribers when they've been cut off due to event overflow.
+	// Optional, set to "" to disable notifications.
+	FailureURL string `json:"failure_url"`
+
+	// Events is the list of regular expressions to match an event type against.
+	Events []string `json:"events"`
+
+	// Matcher type contains values to match against the metadata.
+	Matcher struct {
+		// DeviceID is the list of regular expressions to match device id type against.
+		DeviceID []string `json:"device_id"`
+	} `json:"matcher,omitempty"`
+
+	// Duration describes how long the subscription lasts once added.
+	// Deprecated. User input is ignored and value is always 5m.
+	Duration time.Duration `json:"duration"`
+
+	// Until describes the time this subscription expires.
+	Until time.Time `json:"until"`
+}
