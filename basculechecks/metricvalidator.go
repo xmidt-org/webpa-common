645156741a4e0ce2bba639762f75f5fcca16c685
Kristina Spring <kmspring57@gmail.com>
2020-11-25 11:47:10 -0800

    updated changelog and a few other fixes

--- basculechecks/metricvalidator.go
+++ basculechecks/metricvalidator.go
@@ -123,13 +123,11 @@ func (m MetricValidator) prepMetrics(auth bascule.Authentication) (string, strin
 	if auth.Token == nil {
 		return "", "", "", TokenMissingValues, ErrNoToken
 	}
-	if auth.Request.URL == nil {
-		return "", "", "", TokenMissingValues, ErrNoURL
-	}
 	client := auth.Token.Principal()
 	if auth.Token.Attributes() == nil {
 		return client, "", "", TokenMissingValues, ErrNilAttributes
 	}
+
 	partnerVal, ok := bascule.GetNestedAttribute(auth.Token.Attributes(), PartnerKeys()...)
 	if !ok {
 		return client, "", "", UndeterminedPartnerID, fmt.Errorf("couldn't get partner IDs from attributes using keys %v", PartnerKeys())
@@ -139,6 +137,7 @@ func (m MetricValidator) prepMetrics(auth bascule.Authentication) (string, strin
 		return client, "", "", UndeterminedPartnerID, fmt.Errorf("partner IDs \"%v\" couldn't be cast to string slice: %v", partnerVal, err)
 	}
 	partnerID := DeterminePartnerMetric(partnerIDs)
+
 	if auth.Request.URL == nil {
 		return client, partnerID, "", TokenMissingValues, ErrNoURL
 	}
