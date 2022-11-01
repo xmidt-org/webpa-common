93f90d916591b4e55162ddf6fea2e0a519832578
Owen Cabalceta <denopink@gmail.com>
2022-11-01 16:15:48 -0400

    remove package resource

--- resource/doc.go
+++
@@ -1,8 +0,0 @@
-/*
-Package resource provides common resource-handling functionality.
-
-This package is heavily inspired by frameworks like Spring.  The concept
-is that external resources, such as files or HTTP content, can be configured
-via JSON and then consumed via a common interface.
-*/
-package resource
