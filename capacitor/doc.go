07d35c433cba7513b37b825fc3d1869b15f8c7dd
Owen Cabalceta <denopink@gmail.com>
2022-11-01 16:10:17 -0400

    remove package capacitor

--- capacitor/doc.go
+++
@@ -1,5 +0,0 @@
-/*
- Package capacitor provides a configurable delay for a series of function calls.  A capacitor is discharged
- when it is time to actually invoke the target function.
-*/
-package capacitor
